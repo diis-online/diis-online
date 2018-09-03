033106ba76445f5be3b561177c7682812c260abc
diis-me <41995924+diis-me@users.noreply.github.com>
2018-09-03 20:21:58 +0300

    Create configuration-sample.php

---
+++ www/configuration-sample.php
@@ -0,0 +1,9 @@
+<? // create postgres connection
+$postgres_host = "";
+$postgres_port = "";
+$postgres_database = "";
+$postgres_user = "";
+$postgres_password = "";
+$pg_connection = pg_connect("host=".$postgres_host." port=".$postgres_port." dbname=".$postgres_database." user=".$postgres_user." password=".$postgres_password." options='--client_encoding=UTF8'");
+$status = pg_connection_status($pg_connection);
+if ($status !== PGSQL_CONNECTION_OK): echo "failed database connection"; exit; endif; ?>
