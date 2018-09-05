dc42062fbce7426589255122a989d49f1e9a4686
diis-me <41995924+diis-me@users.noreply.github.com>
2018-09-06 02:35:34 +0300

    Update index.php

--- www/index.php
+++ www/index.php
@@ -38,34 +38,27 @@ echo "</head><body>";
 echo "<amp-install-serviceworker src='https://diis.online/service-worker.js' layout='nodisplay'></amp-install-serviceworker>";
 
 echo "<div id='footer-bar'>";
-
-
+echo "<span id='footer-bar-signin'><i class='material-icons'>account_circle</i> Sign in</span>";
 echo "</div>";
 
 echo "<amp-img id='home-window-logo' alt='⨟' src='diis-logo-20180903.jpg' width='1' height='1' layout='responsive' sizes='(min-width: 300px) 240px, 80vw' amp-fx='parallax' data-parallax-factor='1.5'></amp-img>";
 
-echo "<h1>Coming soon</h1>";
 
+echo "<h1>Coming soon</h1>";
 echo "<p>Diis is the activist's platform for the most challenging stories in the most difficult environments.</p>";
 
 echo "<h2>Unsilenced.</h2>";
-
 echo "<p>We are a small team of journalists and officials based in Baghdad, Erbil, and Istanbul who are commited to creating a safe platform where minority voices in the Middle East can be read loud and clear.</p>";  
 
 echo "<h2>Safety first.</h2>";
-
 echo "<p>Diis is the leader in security and anonymity, with high-level protection of all data and end-to-end encryption for all traffic. Diis is also video-free, ad-free, and mobile-friendly for access on weak connections.</p>";
 
 echo "<h2>Become a publisher.</h2>";
-
-echo "<p class='home-window-list'><b>&#120793;</b><br>Create your account.</p>";
-
-echo "<p class='home-window-list'><b>&#120794;</b><br>Submit your writing.</p>";
-
-echo "<p class='home-window-list'><b>&#120795;</b><br>Wait for approval.</p>";
+echo "<p class='home-window-list' sizes='(min-width: 1140px) 30vw, 90vw'><b>&#120793;</b><br>Create your account.</p>";
+echo "<p class='home-window-list' sizes='(min-width: 1140px) 30vw, 90vw'><b>&#120794;</b><br>Submit your writing.</p>";
+echo "<p class='home-window-list' sizes='(min-width: 1140px) 30vw, 90vw'><b>&#120795;</b><br>Wait for approval.</p>";
 
 echo "<h3>Ready?</h3>";
-
 echo "<a href='?view=register'><span id='home-window-register-button'>Create account</span></a>";
 
 footer();
