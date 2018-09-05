b789a19da7ab4be2dd03842d9d0f78e8b80e1c22
diis-me <41995924+diis-me@users.noreply.github.com>
2018-09-05 13:08:23 +0300

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,21 +1,36 @@
 <? include_once('configuration.php');
 
 echo "<!doctype html><html amp lang='en'><head><meta charset='utf-8'>";
+
 echo "<script async src='https://cdn.ampproject.org/v0.js'></script>";
 echo "<link rel='canonical' href='https://$canonical'>"; // must define canonical url for amp
+
+// PWA manifest
+// https://developers.google.com/web/fundamentals/web-app-manifest/
 echo "<link rel='manifest' href='manifest.json'>";
+
+
+// Include AMP elements
 echo "<style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>";
 echo '<script async custom-element="amp-form" src="https://cdn.ampproject.org/v0/amp-form-0.1.js"></script>';
 echo '<script async custom-element="amp-carousel" src="https://cdn.ampproject.org/v0/amp-carousel-0.1.js"></script>';
 echo '<script async custom-element="amp-fx-collection" src="https://cdn.ampproject.org/v0/amp-fx-collection-0.1.js"></script>';
+echo "<meta name='viewport' content='width=device-width,minimum-scale=1,initial-scale=1'>"; // must define viewport for amp
+
+// General appearance
 echo "<title>Diis</title>";
+echo "<meta name='theme-color' content='#4285f4'>
+echo "<link rel='icon' sizes='192x192' href='icon.png'>";
+echo "<link rel='apple-touch-icon' href='icon.png'>";
+
+// Fonts and CSS style
 echo '<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">';
 echo '<link href="https://fonts.googleapis.com/css?family=Libre+Baskerville" rel="stylesheet">';
 echo '<link href="https://fonts.googleapis.com/css?family=Palanquin" rel="stylesheet">';
-echo "<meta name='viewport' content='width=device-width,minimum-scale=1,initial-scale=1'>"; // must define viewport for amp
 echo "<style amp-custom>";
 include_once('style.css');
 echo "</style>";
+
 echo "</head><body>";
 
 echo "<amp-install-serviceworker src='https://diis.online/service-worker.js' layout='nodisplay'></amp-install-serviceworker>";
