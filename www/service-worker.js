5684ce8d45ff3c1dc64b5a0315d721119c091e91
diis-me <41995924+diis-me@users.noreply.github.com>
2018-09-05 13:38:35 +0300

    Update service-worker.js

--- www/service-worker.js
+++ www/service-worker.js
@@ -1,3 +1,33 @@
 // https://www.ampproject.org/docs/integration/pwa-amp/amp-as-pwa
+// https://developers.google.com/web/fundamentals/primers/service-workers/
 
-// samples
+var CACHE_NAME = 'diis-online-cache-v1';
+var urlsToCache = [
+  '/',
+  '/style.css',
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
