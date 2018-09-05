161485e3b565d563316423417bf05a091eef40e3
diis-me <41995924+diis-me@users.noreply.github.com>
2018-09-06 02:44:08 +0300

    Update index.php

--- www/index.php
+++ www/index.php
@@ -47,19 +47,19 @@ echo "<amp-img id='home-window-logo' alt='⨟' src='diis-logo-20180903.jpg' widt
 echo "<h1 amp-fx='parallax' data-parallax-factor='1.2'>Coming soon</h1>";
 echo "<p>Diis is the activist's platform for the most challenging stories in the most difficult environments.</p>";
 
-echo "<h2 amp-fx='parallax' data-parallax-factor='1.2'>Unsilenced.</h2>";
+echo "<h2 amp-fx='parallax' data-parallax-factor='1.05'>Unsilenced.</h2>";
 echo "<p>We are a small team of journalists and officials based in Baghdad, Erbil, and Istanbul who are commited to creating a safe platform where minority voices in the Middle East can be read loud and clear.</p>";  
 
-echo "<h2 amp-fx='parallax' data-parallax-factor='1.2'>Safety first.</h2>";
+echo "<h2 amp-fx='parallax' data-parallax-factor='1.05'>Safety first.</h2>";
 echo "<p>Diis is the leader in security and anonymity, with high-level protection of all data and end-to-end encryption for all traffic. Diis is also video-free, ad-free, and mobile-friendly for access on weak connections.</p>";
 
-echo "<h2 amp-fx='parallax' data-parallax-factor='1.2'>Become a publisher.</h2>";
+echo "<h2>Become a publisher.</h2>";
 echo "<p class='home-window-list'><b>&#120793;</b><br>Create your account.</p>";
 echo "<p class='home-window-list'><b>&#120794;</b><br>Submit your writing.</p>";
 echo "<p class='home-window-list'><b>&#120795;</b><br>Wait for approval.</p>";
 
 echo "<h3>Ready?</h3>";
-echo "<a href='?view=register'><span id='home-window-register-button' amp-fx='parallax' data-parallax-factor='1.3'>Create account</span></a>";
+echo "<a href='?view=register'><span id='home-window-register-button' amp-fx='parallax'>Create account</span></a>";
 
 footer();
 
