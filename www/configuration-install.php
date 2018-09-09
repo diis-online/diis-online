180aeab97ea700664ef116e719172d946f36e76c
diis-me <41995924+diis-me@users.noreply.github.com>
2018-09-09 15:58:44 +0300

    Update configuration-install.php

--- www/configuration-install.php
+++ www/configuration-install.php
@@ -1,54 +1,32 @@
 <? include_once("configuration.php");
 
-//
-//
-// table schema for system configuration, e.g. for reCAPTCHA
-$table_name = "system_configuration";
-$table_schema = [
+$tables_array = [];
+
+// Table schema for system configuration, e.g. for reCAPTCHA
+$tables_array['system_configuration'] = [
 	"configuration_id" => "INTEGER",
 	"configuration_category" => "VARCHAR(50)",
 	"configuration_frontend" => "VARCHAR(100)",
 	];
 
-
-
-//
-//
-// table schema for translatable u.x. elements
-$table_name = "translatable_elements";
-$table_schema = [
+// Table schema for translatable u.x. elements
+$tables_array['translatable_elements'] = [
 	"element_name" => "VARCHAR(20)", // the name of the translatable u.x. element
 	];
 foreach ($system_languages as $language_key => $language_frontend):
-	$table_schema[$language_key] = "VARCHAR(500)";
+	$tables_array['translatable_elements'][$language_key] = "TEXT";
 	endforeach;
 
-$translatable_elements = file_get_contents("../translatable-elements.txt", FILE_USE_INCLUDE_PATH);
-// parse $translatable_elements and insert into the database
-
-
-
-//
-//
-// table schema for username options
-$table_name = "username_options";
-$table_schema = [
-  "option_id" => "INTEGER",
+// Table schema for username options
+$tables_array['username_options'] = [
+	"option_id" => "INTEGER",
 	];
 foreach ($system_languages as $language_key => $language_frontend):
-	$table_schema[$language_key] = "VARCHAR(100)";
+	$tables_array['username_options'][$language_key] = "VARCHAR(100)";
 	endforeach;
 
-$username_options = file_get_contents("../username-options.txt", FILE_USE_INCLUDE_PATH);
-// parse out the options and insert into the database
-
-
-
-//
-//
-// table schema for users, including status
-$table_name = "users";
-$table_schema = [
+// Table schema for users, including status
+$tables_array['users'] = [
 	"user_id" => "INTEGER", // The unique user id
 	"username_one" => "INTEGER", // The first adjective of their username
 	"username_two" => "INTEGER", // The second adjective of their username
@@ -60,13 +38,8 @@ $table_schema = [
 	"user_hold" => "INTEGER", // UNIX timestamp of how long until the user can log in again, or indefinite
 	];
 
-
-
-//
-//
-// table schema for shares
-$table_name = "shares_main";
-$table_schema = [
+// Table schema for shares
+$tables_array['shares_main'] = [
 	"content_id" => "INTEGER",
 	"author_id" => "INTEGER",
 	"content_language" => "VARCHAR(20)",
@@ -78,13 +51,8 @@ $table_schema = [
 	"content_published" => "VARCHAR(20)", // UNIX timestamp of when the content is published
 	];
 
-
-
-//
-//
-// table schema for shares access — all these users will have access, in addition to the author
-$table_name="shares_access";
-$table_schema = [
+// Table schema for shares access — all these users will have access, in addition to the author
+$tables_array['shares_access'] = [
 	"access_id" => "INTEGER", // Unique ID of their access ... Should be a random digit
 	"user_id" => "INTEGER", // The user that has the access
 	"content_id" => "INTEGER", // The content that the user has access to
@@ -92,13 +60,8 @@ $table_schema = [
 	"access_time" => "INTEGER", // UNIX timestamp of when the change was made
 	];
 
-
-
-//
-//
-// table schema for archiving any work on shares
-$table_name = "shares_archive";
-$table_schema = [
+// Table schema for archiving any work on shares
+$tables_array['shares_archive'] = [
 	"content_archive_id" => "INTEGER", // Unique ID of the archived work
 	"user_id" => "INTEGER", // The user making the change
 	"change_field" => "VARCHAR(20)", // The field where the change is being made.
@@ -106,16 +69,25 @@ $table_schema = [
 	"change_time" => "INTEGER", // UNIX timestamp of when the change was made
 	];
 
-
-
-//
-//
-// table schema for internal shares annotations
-$table_name = "shares_annotations";
-$table_schema = [
+// Table schema for internal shares annotations
+$tables_array['shares_annotations'] = [
 	"annotation_id" => "INTEGER", // Unique ID of the annotation
 	"content_id" => "INTEGER", // ID of the content being annotated
 	"user_id" => "INTEGER", // ID of the user making the annotation
 	"annotation_text" => "TEXT", // Content of the annotation
 	"annotation_timestamp" => "VARCHAR(20)", // UNIX timestamp of when the annotation was made
-	]; ?>
+	]; 
+
+foreach ($tables_array as $table_name => $table_schema):
+	generate_table($table_name, $table_schema);
+	endforeach;
+
+// Fill in translatable elements...
+$translatable_elements = file_get_contents("../translatable-elements.txt", FILE_USE_INCLUDE_PATH);
+// parse $translatable_elements and insert into the database
+
+// Fill in username options...
+$username_options = file_get_contents("../username-options.txt", FILE_USE_INCLUDE_PATH);
+// parse out the options and insert into the database
+
+exit; ?>
