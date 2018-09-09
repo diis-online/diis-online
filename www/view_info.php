fb24517d77061e6df0cea3cd1083e7e6cd092536
diis-me <41995924+diis-me@users.noreply.github.com>
2018-09-09 10:59:54 +0300

    Update view_info.php

--- www/view_info.php
+++ www/view_info.php
@@ -2,19 +2,19 @@
 echo "<p>Diis is the activist's platform for the most challenging stories in the most difficult environments.</p>";
 echo "<p>We provide a system to get your writing out where it is needed most.</p>";
 
-echo "<h2 amp-fx='parallax' data-parallax-factor='1.1'>Unsilenced.</h2>";
+echo "<h2 amp-fx='parallax' data-parallax-factor='1.05'>Unsilenced.</h2>";
 echo "<p>We are journalists and officials based in Baghdad, Erbil, and Istanbul who are commited to creating a secure platform for being heard.</p>";
 echo "<p>Want to join? Keep reading.</p>"; 
 
-echo "<h2 amp-fx='parallax' data-parallax-factor='1.15'>Safety first.</h2>";
-echo "<p amp-fx='parallax' data-parallax-factor='1.15'>Diis is the leader in security and anonymity, with high-level protection of all data and end-to-end encryption for all traffic. Diis is also video-free, ad-free, and mobile-friendly for access on weak connections.</p>";
+echo "<h2 amp-fx='parallax' data-parallax-factor='1.05'>Safety first.</h2>";
+echo "<p>Diis is the leader in security and anonymity, with high-level protection of all data and end-to-end encryption for all traffic. Diis is also video-free, ad-free, and mobile-friendly for access on weak connections.</p>";
 
 // If the user is not logged in
 
-echo "<amp-img id='home-window-logo' alt='⨟' src='diis-logo-20180903.jpg' width='1' height='1' layout='responsive' sizes='(min-width: 300px) 150px, 50vw' amp-fx='parallax' data-parallax-factor='1.05'></amp-img>";
+echo "<amp-img id='home-window-logo' alt='⨟' src='diis-logo-20180903.jpg' width='1' height='1' layout='responsive' sizes='(min-width: 300px) 150px, 50vw' amp-fx='parallax' data-parallax-factor='1.1'></amp-img>";
 
-echo "<h2 amp-fx='parallax' data-parallax-factor='1.05'>Become a publisher.</h2>";
-echo "<p amp-fx='parallax' data-parallax-factor='1.05'>Create your account and share to the world. Our editors and translators are ready to support you. If your writing is approved, then it will be ready for publication.</p>";
+echo "<h2 amp-fx='parallax' data-parallax-factor='1.1'>Become a publisher.</h2>";
+echo "<p amp-fx='parallax' data-parallax-factor='1.1'>Create your account and share to the world. Our editors and translators are ready to support you. If your writing is approved, then it will be ready for publication.</p>";
 echo "<a href='?view=register'><span id='home-window-register-button' amp-fx='parallax'>Create account</span></a>";
 
 footer(); ?>
