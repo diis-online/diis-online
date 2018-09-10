778da2ae92e51ab63813e8b65731193544b4abbe
diis-me <41995924+diis-me@users.noreply.github.com>
2018-09-10 15:19:27 +0300

    Update index.php

--- www/index.php
+++ www/index.php
@@ -65,7 +65,7 @@ function body($title="Diis", $include=null) {
 	if (!(empty($login_status))):
 	
 		echo "<amp-date-countdown timestamp-seconds='".($login_status['user_login_time']+5)."' layout='fixed-height' height='100' when-ended='stop on='timeout: timeout-overlay-open.start'>";
-		echo "<template type='amp-mustache'><div id='login-hourglass-countdown'>{{m}} minutes, {{s}} seconds left on page</div></template>";
+		echo "<template type='amp-mustache'><div id='login-hourglass-countdown' animate-in='fade-in' animate-in-duration='2s'>{{m}} minutes, {{s}} seconds left on page</div></template>";
 		echo "</amp-date-countdown>";
 	
 		echo "<div id='login-hourglass-timeout'>Session maybe expired.</div>";
