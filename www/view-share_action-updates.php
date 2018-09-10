f6c1e402f2a7458f8de22d5160535adfe5c6a691
diis-me <41995924+diis-me@users.noreply.github.com>
2018-09-10 23:55:41 +0300

    Update view-share_action-updates.php

--- www/view-share_action-updates.php
+++ www/view-share_action-updates.php
@@ -1,6 +1,15 @@
 <? if (empty($script_code)): exit; endif;
 
-// Output the latest annotations as JSON
+$json_result = [
+  "items" => [
+		[ "author"=>"111", "timestamp"=>"2018 Jan 1", "contents"=>"Contents of first item"],
+		[ "author"=>"222", "timestamp"=>"2018 Feb 02", "contents"=>"Contents of second item"],
+		[ "author"=>"333", "timestamp"=>"2018 Mar 3", "contents"=>"Contents of third item"],
+		[ "author"=>"444", "timestamp"=>"2018 Apr 4", "contents"=>"Contents of fourth item"],
+		[ "author"=>"555", "timestamp"=>"2018 May 5", "contents"=>"Contents of fifth item"],
+		],
+	];
 
+echo json_encode($json_result);
 
-?>
+exit; ?>
