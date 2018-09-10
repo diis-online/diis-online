c7d6b8255cff806eb6019ecc779e2f440b5091b4
diis-me <41995924+diis-me@users.noreply.github.com>
2018-09-10 23:50:36 +0300

    Update view-share_action-edit.php

--- www/view-share_action-edit.php
+++ www/view-share_action-edit.php
@@ -3,12 +3,13 @@
 echo "<form target='_top' action-xhr='?view=share&share=". $share_info['share_id'] ."&action=save' method='post'>";
 echo "<input type='hidden' name='share_id' value='".$share_info['share_id']."'>";
 
-echo "<textarea name='body' placeholder='Write here...' required></textarea>";
+echo "<textarea name='body' placeholder='Write here...' id='edit-window-textarea' required></textarea>";
 
 // save draft button.... this will just update the draft...
 
-// if the user is the admin doing the review then give a publish live button... this will update the draft and published item...
-
+if (($share_info['author_id'] !== $login_status['user_id']) && (in_array{$login_status['level'], ["administrator", "editor"]))):
+	echo "<button id='edit-window-publish'>Publish</button>";
+	endif;
 // View all annotations
 
 echo "<div submit-success><template type='amp-mustache'>Success!</template></div>";
