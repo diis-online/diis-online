22ced9a5fecc608dad5d01bbd9c41f8c0b3f9faa
diis-me <41995924+diis-me@users.noreply.github.com>
2018-09-11 23:18:58 +0300

    Update view-info.php

--- www/view-info.php
+++ www/view-info.php
@@ -3,8 +3,6 @@
 echo "<h1 amp-fx='parallax' data-parallax-factor='1.2'>Coming soon</h1>";
 echo "<p>Diis is the activist's platform for the most challenging stories in the most difficult environments. After you publish, we work to get your writing where it is needed.</p>";
 
-echo "<amp-img id='home-window-logo' alt='⨟' src='reading-eye-icon.jpg' width='600' height='259' layout='responsive' sizes='(min-width: 300px) 255px, 85vw' amp-fx='parallax' data-parallax-factor='1.04'></amp-img>";
-
 echo "<h2 amp-fx='parallax' data-parallax-factor='1.05'>Unsilenced.</h2>";
 echo "<p>We are journalists and officials based in Baghdad, Erbil, and Istanbul who are commited to creating a secure platform for being heard.</p>";
 echo "<p>Want to join?.</p>";
@@ -12,10 +10,8 @@ echo "<p>Want to join?.</p>";
 echo "<amp-img id='home-window-logo' alt='⨟' src='diis-logo-20180903.jpg' width='1' height='1' layout='responsive' sizes='(min-width: 300px) 150px, 50vw' amp-fx='parallax' data-parallax-factor='1.04'></amp-img>";
 
 echo "<h2 amp-fx='parallax' data-parallax-factor='1.05'>Become a publisher.</h2>";
+echo "<p amp-fx='parallax' data-parallax-factor='1.03'>Create your account and share to the world. Our editors and translators are ready to support you. If your writing is approved, then it will be ready for publication.</p>";
 echo "<a href='?view=register' amp-fx='parallax' data-parallax-factor='0.98'><span id='home-window-register-button'>Create account</span></a>";
-echo "<p amp-fx='parallax' data-parallax-factor='0.97'>Create your account and share to the world. Our editors and translators are ready to support you. If your writing is approved, then it will be ready for publication.</p>";
-
-echo "<amp-img id='home-window-logo' alt='⨟' src='feed-incoming-icon.jpg' width='600' height='334' layout='responsive' sizes='(min-width: 300px) 255px, 85vw' amp-fx='parallax' data-parallax-factor='1.04'></amp-img>";
 
 echo "<h2>Safety first.</h2>";
 echo "<p>Diis is the leader in security and anonymity, with high-level protection of all data and end-to-end encryption for all traffic. Diis is also video-free, ad-free, and mobile-friendly for access on weak connections.</p>";
