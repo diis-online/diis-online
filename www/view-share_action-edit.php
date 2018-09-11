a8e9fec20b1289c878cae07c0ace36bee4df03c2
diis-me <41995924+diis-me@users.noreply.github.com>
2018-09-11 09:06:22 +0300

    Update view-share_action-edit.php

--- www/view-share_action-edit.php
+++ www/view-share_action-edit.php
@@ -3,9 +3,22 @@
 echo "<form target='_top' action-xhr='?view=share&share=". $share_info['share_id'] ."&action=xfr' method='post'>";
 echo "<input type='hidden' name='share_id' value='".$share_info['share_id']."'>";
 
-echo "<textarea name='body' placeholder='Write here...' id='edit-window-textarea' required></textarea>";
+if (!(empty($share_info['content_approved']))):
+
+	// Button to toggle on the show-more approved content
+
+	echo "<button id='edit-window-publish'>Reset draft</button>";
+
+	echo "<hr id='edit-window-stroke'>";
+
+	endif;
+
+
+echo "<textarea name='body' placeholder='Write here...' id='edit-window-textarea' required>sdfd</textarea>";
 
 echo "<button id='edit-window-publish'>Save draft</button>";
+
+echo "<hr id='edit-window-stroke'>";
 
 if (($share_info['author_id'] !== $login_status['user_id']) && (in_array($login_status['level'], ["administrator", "editor"]))):
 	echo "<button id='edit-window-publish'>Publish</button>";
@@ -18,12 +31,16 @@ echo "</form>";
 echo "<form target='_top' action-xhr='?view=share&share=". $share_info['share_id'] ."&action=save' method='post'>";
 echo "</form>";
 
-echo "<button on='tap:annotations-list.refresh'>Refresh annotations</button>";
-echo "<amp-list id='annotations-list' src='https://diis.online?view=share&share=".$share_request."&action=updates'>";
-echo "<div placeholder>Loading ...</div>";
-echo "<div fallback>Failed to load data.</div>";
+echo "<hr id='edit-window-stroke'>";
+
+echo "<span class='edit-window-annotations-header'>Annotations</span>";
+echo "<button on='tap:annotations-list.refresh' id='edit-window-annotations-button'>Refresh annotations</button>";
+
+echo "<amp-list id='annotations-list' max-items='10' src='https://diis.online?view=share&share=".$share_request."&action=updates'>";
+echo "<div id='edit-window-annotations-placeholder' placeholder>Loading ...</div>";
+echo "<div id='edit-window-annotations-fallback' fallback>Failed to load data.</div>";
 echo "<template type='amp-mustache'>";
-	echo "<div class='annotations-list-item'><span>Author:{{user_id}}</span><span>Time: {{annotation_timestamp}}</span><span>Contents: {{annotation_text}}</div>";
+	echo "<div class='edit-window-annotations-list-item'><span>Author:{{user_id}}</span><span>Time: {{annotation_timestamp}}</span><span>Contents: {{annotation_text}}</div>";
 echo "</template></amp-list>";
 	
 echo "</form>"; ?>
