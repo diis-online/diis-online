476da548b15594661966e6a6494885ccb554b1a4
diis-me <41995924+diis-me@users.noreply.github.com>
2018-09-11 10:52:58 +0300

    Update index.php

--- www/index.php
+++ www/index.php
@@ -65,7 +65,7 @@ function body($title="Diis", $include=null) {
 
 	if (!(empty($login_status))):
 	
-		echo "<amp-date-countdown timestamp-seconds='".($login_status['user_login_time']+5)."' layout='fixed-height' height='100' when-ended='stop' on='timeout: timeout-overlay-open.start'>";
+		echo "<amp-date-countdown timestamp-seconds='".($login_status['user_login_time']+7200)."' layout='fixed-height' height='100' when-ended='stop' on='timeout: timeout-overlay-open.start'>";
 		echo "<template type='amp-mustache'><div id='login-hourglass-countdown'>{{m}} minutes, {{s}} seconds left on page</div></template>";
 		echo "</amp-date-countdown>";
 	
@@ -160,7 +160,7 @@ if ($view_request == "install"):
 $login_status = [
 	"user_id" => "testing",
 	"level" => "testing",
-	"user_login_time" => time(),
+	"user_login_time" => (time()-5430),
 	];
 
 $share_info = [];
