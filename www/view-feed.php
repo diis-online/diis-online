eb10f8ea138097ccba5202b16f6512cb9f7e55db
diis-me <41995924+diis-me@users.noreply.github.com>
2018-09-11 23:24:26 +0300

    Update view-feed.php

--- www/view-feed.php
+++ www/view-feed.php
@@ -8,13 +8,13 @@ echo "<p>Diis is the activist's platform for the most challenging stories in the
 echo "<h2 amp-fx='parallax' data-parallax-factor='1.05'>Unsilenced.</h2>";
 echo "<p>We are journalists and officials based in Baghdad, Erbil, and Istanbul who are commited to creating a secure platform for being heard.</p>";
 
-if (empty($login_status))):
+if (empty($login_status)):
 	echo "<p>Want to join?.</p>";
 	endif;
 
 echo "<amp-img id='home-window-logo' alt='⨟' src='diis-logo-20180903.jpg' width='1' height='1' layout='responsive' sizes='(min-width: 300px) 150px, 50vw' amp-fx='parallax' data-parallax-factor='1.04'></amp-img>";
 
-if (empty($login_status))):
+if (empty($login_status)):
 	echo "<h2 amp-fx='parallax' data-parallax-factor='1.05'>Become a publisher.</h2>";
 	echo "<p amp-fx='parallax' data-parallax-factor='1.03'>Create your account and share to the world. Our editors and translators are ready to support you. If your writing is approved, then it will be ready for publication.</p>";
 	echo "<a href='?view=register' amp-fx='parallax' data-parallax-factor='0.98'><span id='home-window-register-button'>Create account</span></a>";
