f57105603e449b76c68a2403513176b29e745108
diis-me <41995924+diis-me@users.noreply.github.com>
2018-09-11 18:10:17 +0300

    Update configuration-sample.php

--- www/configuration-sample.php
+++ www/configuration-sample.php
@@ -3,12 +3,4 @@ $postgres_host = "";
 $postgres_port = "";
 $postgres_database = "";
 $postgres_user = "";
-$postgres_password = "";
-
-// supported languages
-$languages = [
-  "arabic" => "عربي",
-  "english" => "English",
-  "sorani" => "سۆرانی",
-  "turkish" => "Türkçe",
-  ]; ?>
+$postgres_password = ""; ?>
