4b5c8c8d24ce44ffd146e347abc00838bb34cbad
diis-me <41995924+diis-me@users.noreply.github.com>
2018-09-11 11:31:45 +0300

    Update view-share_action-edit.php

--- www/view-share_action-edit.php
+++ www/view-share_action-edit.php
@@ -7,11 +7,11 @@ if (!(empty($share_info['content_approved']))):
 
 	// Button to toggle on the show-more approved content
 
-	echo "<button id='edit-window-show-approved-button' on='tap: edit-window-approved-post-lightbox'>Show approved post</button>";
+	echo "<button id='edit-window-show-approved-button' on='tap: edit-window-approved-post-lightbox.open'>Show approved post</button>";
 
 	echo "<amp-lightbox id='edit-window-approved-post-lightbox' layout='nodisplay'>";
 	echo "<button id='edit-window-close-approved-button' on='tap: edit-window-approved-post-lightbox.close'>Close</button>";
-	echo $share_info['content_approved'];
+	echo $share_info['content_approved']."jkdfgnsdjklgnsdklfjgn";
 	echo "</amp-lightbox>";
 
 	echo "<button id='edit-window-publish'>Reset draft</button>";
