fe4da1bcb5ca66b0e41c0ab7f21af0273cdbb795
diis-me <41995924+diis-me@users.noreply.github.com>
2018-09-11 16:06:18 +0300

    Update view-share_action-xfr.php

--- www/view-share_action-xfr.php
+++ www/view-share_action-xfr.php
@@ -1,13 +1,16 @@
 <? if (empty($script_code)): exit; endif;
 
-$login_status = [
-	"user_id" => "testing",
-	"level" => "testing",
-	];
-
-if (empty($login_status)): echo json_encode(["result"=>"success", "message"=>"Login failed."]); exit; endif;
-
 if (empty($_POST['share_id'])): echo json_encode(["result"=>"success", "message"=>"Share not specified."]); exit; endif;
+
+$content_draft = $_POST['content_draft'] ?? null;
+$content_draft = trim($content_draft);
+
+if (empty($content_draft)): echo json_encode(["result"=>"success", "message"=>"Content empty."]); exit; endif;
+
+$content_status = $_POST['content_status'] ?? null;
+
+if (empty($content_status)): echo json_encode(["result"=>"success", "message"=>"Status empty."]); exit; endif;
+
 
 $share_info = [];
 if ($_POST['share_id'] == "create"):
@@ -25,17 +28,90 @@ else:
 
 if (empty($share_info)): echo json_encode(["result"=>"success", "message"=>"Content does not exist."]); exit; endif;
 
-$permission_temp = 0;
-if ($login_status['user_id'] == $share_info['author_id']): $permission_temp = 1; endif;
-if (in_array($login_status['level'], ["administrator", "editor"])): $permission_temp = 1; endif;
+$change_temp = 0;
 
-if ($permission_temp == 0): echo json_encode(["result"=>"success", "message"=>"Login failed."]); exit; endif;
+// If a change has happened to the draft...
+if ($content_draft !== $share_info['content_draft']):
 
-// If there is an administrator
+	// Is it being saved as a draft or as approved content
+	$values_temp = [
+		"share_id" => $share_info['share_id'],
+		"author_id" => $share_info['author_id'],
+		"content_draft" => $content_draft,
+		];
+		
+	// also, add to archive
+	$values_temp = [
+		"content_archive_id" => random_number(10),
+		"user_id" => $login_status['user_id'],
+		"change_field" => "content_draft",
+		"change_value" => $content_draft,
+		"change_time" => time(),
+		];
+	
+	$change_temp = 1;
+	
+	endif;
 
-// Is it being saved as a draft or as approved content
+// If there is an administrator AND not the author AND we publish...
+if ( ($login_status['user_id'] !== $share_info['author_id']) && ($content_status == "published") ):
 
-// Check posted data and do SQL work
+	$values_temp = [
+		"share_id" => $share_info['share_id'],
+		"content_published" => $content_draft,
+		];
+
+	// also, add to archive
+	$values_temp = [
+		"content_archive_id" => random_number(10),
+		"user_id" => $login_status['user_id'],
+		"change_field" => "content_published",
+		"change_value" => $content_draft,
+		"change_time" => time(),
+		];
+		
+	if (empty($shared_info['published_time'])):
+
+		$published_time = $_POST['published_time'] ?? time();
+
+		$values_temp = [
+			"share_id" => $share_info['share_id'],
+			"published_time" => $published_time,
+			];
+		
+		// also, add to archive
+		$values_temp = [
+			"content_archive_id" => random_number(10),
+			"user_id" => $login_status['user_id'],
+			"change_field" => "published_time",
+			"change_value" => $published_time,
+			"change_time" => time(),
+			];
+			
+		endif;
+		
+	$change_temp = 1;
+
+	endif;
+
+// If a change occurred to the content, then also update the status...
+if ( ($change_temp == 1) && ($share_info['content_status'] !== $content_status) ):	
+	
+	$values_temp = [
+		"share_id" => $share_info['share_id'],
+		"content_status" => $content_status,
+		];
+			
+	// also, add to archive
+	$values_temp = [
+		"content_archive_id" => random_number(10),
+		"user_id" => $login_status['user_id'],
+		"change_field" => "content_status",
+		"change_value" => $content_status,
+		"change_time" => time(),
+		];
+			
+	endif;
 
 // largely thanks to https://stackoverflow.com/questions/43422257/amp-form-submission-redirect-or-response
 header("Content-type: application/json");
@@ -48,5 +124,5 @@ header("AMP-Access-Control-Allow-Source-Origin: https://diis.online");
 // if no redirect
 	// header("Access-Control-Expose-Headers: AMP-Access-Control-Allow-Source-Origin");
 	// and end headers here
-header("AMP-Redirect-To: https://diis.online/?view=share&action=edit&share=".$share_info['share_id']);
+header("AMP-Redirect-To: https://diis.online/?view=share&action=edit&share=".$share_info['share_id']."&action=edit");
 header("Access-Control-Expose-Headers: AMP-Redirect-To, AMP-Access-Control-Allow-Source-Origin"); ?>
