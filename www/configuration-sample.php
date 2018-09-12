e298526b1d209fdba1a9bd9ecb867709cdaed0ec
diis-me <41995924+diis-me@users.noreply.github.com>
2018-09-12 22:50:12 +0300

    Update configuration-sample.php

--- www/configuration-sample.php
+++ www/configuration-sample.php
@@ -5,6 +5,11 @@ $postgres_database = "";
 $postgres_user = "";
 $postgres_password = "";
 
+// Change this to "enabled" and bo to https://diis.online/?view=install
+// This will install the first time and allow you to create an administrator
+// Once you have finished that that then change this back to "disabled"
+$allow_install = "disabled";
+
 // This should not need to be changed, except to disable languages...
 $languages = [ 
   "en" => "English",
