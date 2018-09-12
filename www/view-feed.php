5603de1b9cbdf61a6cc2b7b5f4ffe28a3c6d6972
diis-me <41995924+diis-me@users.noreply.github.com>
2018-09-12 15:25:13 +0300

    Update view-feed.php

--- www/view-feed.php
+++ www/view-feed.php
@@ -2,25 +2,25 @@
 
 //
 
-echo "<h1 amp-fx='parallax' data-parallax-factor='1.2'>Coming soon.</h1>";
-echo "<p>Diis is the activist's platform for the most challenging stories in the most difficult environments. After you publish, we work to get your writing where it is needed.</p>";
+echo "<h1 amp-fx='parallax' data-parallax-factor='1.2'>".$translatable_elements['coming-soon'][$language_request]."</h1>";
+echo "<p>".$translatable_elements['mission-statement'][$language_request]."</p>";
 
-echo "<h2 amp-fx='parallax' data-parallax-factor='1.05'>Unsilenced.</h2>";
-echo "<p>We are journalists and officials based in Baghdad, Erbil, and Istanbul who are commited to creating a secure platform for being heard.</p>";
+echo "<h2 amp-fx='parallax' data-parallax-factor='1.05'>".$translatable_elements['coming-soon'][$language_request]."</h2>";
+echo "<p>".$translatable_elements['who-we-are'][$language_request]."</p>";
 
 if (empty($login_status)):
-	echo "<p>Want to join?.</p>";
+	echo "<p></p>";
 	endif;
 
 echo "<amp-img id='home-window-logo' alt='⨟' src='diis-logo-20180903.jpg' width='1' height='1' layout='responsive' sizes='(min-width: 300px) 150px, 50vw' amp-fx='parallax' data-parallax-factor='1.04'></amp-img>";
 
 if (empty($login_status)):
-	echo "<h2 amp-fx='parallax' data-parallax-factor='1.05'>Become a publisher.</h2>";
-	echo "<p amp-fx='parallax' data-parallax-factor='1.03'>Create your account and share to the world. Our editors and translators are ready to support you. If your writing is approved, then it will be ready for publication.</p>";
-	echo "<a href='?view=register' amp-fx='parallax' data-parallax-factor='0.98'><span id='home-window-register-button'>Create account</span></a>";
+	echo "<h2 amp-fx='parallax' data-parallax-factor='1.05'>".$translatable_elements['become-a-publisher'][$language_request]."</h2>";
+	echo "<p amp-fx='parallax' data-parallax-factor='1.03'>".$translatable_elements['publisher-join-instructions'][$language_request]."</p>";
+	echo "<a href='?view=register' amp-fx='parallax' data-parallax-factor='0.98'><span id='home-window-register-button'>".$translatable_elements['create-account'][$language_request]."</span></a>";
 	endif;
 
-echo "<h2>Safety first.</h2>";
-echo "<p>Diis is the leader in security and anonymity, with high-level protection of all data and end-to-end encryption for all traffic. Diis is also video-free, ad-free, and mobile-friendly for access on weak connections.</p>";
+echo "<h2>".$translatable_elements['safety-first'][$language_request]."</h2>";
+echo "<p>".$translatable_elements['safety-description'][$language_request]."</p>";
 
 footer(); ?>
