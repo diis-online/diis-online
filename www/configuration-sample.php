46160e80b0da3baabf3f179991c122ae56507ec1
diis-me <41995924+diis-me@users.noreply.github.com>
2018-09-12 22:27:36 +0300

    Update configuration-sample.php

--- www/configuration-sample.php
+++ www/configuration-sample.php
@@ -1,6 +1,14 @@
-<? // create postgres connection
+<? // Parameters for Postgres connection...
 $postgres_host = "";
 $postgres_port = "";
 $postgres_database = "";
 $postgres_user = "";
-$postgres_password = ""; ?>
+$postgres_password = "";
+
+// This should not need to be changed, except to disable languages...
+$languages = [ 
+  "en" => "English",
+	"ar" => "عربي",
+	"ku" => "سۆرانی",
+	"tr" => "Türkçe",
+	]; ?>
