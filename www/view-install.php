3f35042915933baddf8936ef812bc256f0e8483f
diis-me <41995924+diis-me@users.noreply.github.com>
2018-09-19 12:34:16 +0300

    Update view-install.php

--- www/view-install.php
+++ www/view-install.php
@@ -197,7 +197,7 @@ elseif ($admin_temp !== 1):
 
 	echo "<h2>Create administrator account.</h2>";
 
-	echo "<form target='_top' action-xhr='?view=install&action=xhr&language=".$language_request."' method='post'>";
+	echo "<form target='_top' action-xhr='https://diis.online/?view=install&action=xhr&language=".$language_request."' method='post'>";
 	
 	// Thing to say whether or not it was successful and to go to homepage...
 
