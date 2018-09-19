85fa0b872521a4b040ff1aaa002e9063c5ab5aeb
diis-me <41995924+diis-me@users.noreply.github.com>
2018-09-19 12:38:07 +0300

    Update view-install_action-xhr.php

--- www/view-install_action-xhr.php
+++ www/view-install_action-xhr.php
@@ -6,6 +6,19 @@
 
 // Create account...
 
+header("Content-type: application/json");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Origin: https://".$domain);
+header("AMP-Access-Control-Allow-Source-Origin: https://diis.online");
+// if failure
+	// header("HTTP/1.0 412 Precondition Failed", true, 412);
+	// and end headers here
+// if no redirect
+	header("Access-Control-Expose-Headers: AMP-Access-Control-Allow-Source-Origin");
+	// and end headers here
+// header("AMP-Redirect-To: https://".$domain."/".$_POST['page']);
+// header("Access-Control-Expose-Headers: AMP-Redirect-To, AMP-Access-Control-Allow-Source-Origin");
+
 json_output("success", "Helloooo2o".$_POST['pin'].$_POST['username']);
 
 exit; ?>
