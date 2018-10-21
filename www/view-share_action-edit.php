b3fe33186d139dde1f2b5e17c51cfe6c7c7ac112
diis-me <41995924+diis-me@users.noreply.github.com>
2018-10-21 19:15:46 +0300

    Update view-share_action-edit.php

--- www/view-share_action-edit.php
+++ www/view-share_action-edit.php
@@ -35,11 +35,10 @@ if (!(empty($share_info['content_approved']))):
 	echo "<button id='edit-window-reset-button' type='reset'><i class='material-icons'>cancel_presentation</i> Reset draft</button>";
 	endif;
 
+echo "<input id='edit-window-save-button' type='submit' name='content_status' value='Save draft'>";
 
 echo "<input type='hidden' name='content_status' value='Hidden content-status button'>";
 echo "<input type='submit' name='content_draft' value='Testing this draft'>";
-
-echo "<input id='edit-window-save-button' type='submit' name='content_status' value='Save draft'>";
 
 // We need to add something about setting the relationship
 
