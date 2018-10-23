5a815e0e2ed5d3fd432725e1ca4984ae7b461915
diis-me <41995924+diis-me@users.noreply.github.com>
2018-10-23 18:26:20 +0300

    Update view-share_action-xhr.php

--- www/view-share_action-xhr.php
+++ www/view-share_action-xhr.php
@@ -14,7 +14,7 @@ $content_status_array = [
 	];
 $content_status = $_POST['content_status'] ?? null;
 $content_status = $content_status_array[$content_status] ?? null;
-if (empty($content_status)): json_output("failure", "Status empty.".implode($_POST)); endif;
+if (empty($content_status)): json_output("failure", "Status empty."); endif;
 
 
 $share_info = [];
