32950a0a4292f911a676f8148d02de06bcab26f8
diis-me <41995924+diis-me@users.noreply.github.com>
2018-10-23 18:08:15 +0300

    Update view-share_action-edit.php

--- www/view-share_action-edit.php
+++ www/view-share_action-edit.php
@@ -21,7 +21,7 @@ echo "<input type='hidden' name='share_id' value='".$share_info['share_id']."'>"
 // Display information drop-down: Publishing on Diis is as easy as ❶❷❸: 1) Share what is on your mind. 2) Save your drafts. 3) Submit it for publication review.
 
 
-echo "<div id='edit-window-form-not-submitted'>No changes saved yet.</div>";
+echo "<div id='edit-window-form-not-submitted'>No new changes saved yet.</div>";
 echo "<div submit-success><template type='amp-mustache'>Saved <amp-timeago id='edit-window-form-submit-timeago' layout='responsive' height='20' width='100' datetime='{{{time}}}' locale='en'>{{{time}}}</amp-timeago> {{{message}}}</template></div>";
 echo "<div submit-error><template type='amp-mustache'>Not saved. Last tried <amp-timeago id='edit-window-form-submit-timeago' layout='responsive' height='20' width='100' datetime='{{{time}}}' locale='en'>{{{time}}}</amp-timeago>. {{{message}}}</template></div>";
 echo "<div submitting><template type='amp-mustache'>Saving...</template></div>";
@@ -45,7 +45,7 @@ echo "</div>";
 
 if (($share_info['author_id'] !== $login_status['user_id']) && (in_array($login_status['level'], ["administrator", "editor"]))):
 	echo "<hr class='edit-window-stroke'>";
-	echo "<input id='edit-window-publish-button' type='submit' name='content_status' value='Save and publish online'>";
+	echo "<input id='edit-window-publish-button' type='submit' name='content_status' value='Save and publish online' on='tap:edit-window-post-alignment.toggle'>";
 elseif ( !(empty($share_info['content_draft'])) && ($share_info['content_status'] !== "pending") ):
 	echo "<hr class='edit-window-stroke'>";
 	echo "<input id='edit-window-submit-button' type='submit' name='content_status' value='Submit for publication'>";
