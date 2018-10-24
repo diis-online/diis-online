492f16a18b081b67858050b95e31ea14e8231f10
diis-me <41995924+diis-me@users.noreply.github.com>
2018-10-24 11:11:20 +0300

    Update view-share_action-edit.php

--- www/view-share_action-edit.php
+++ www/view-share_action-edit.php
@@ -12,7 +12,7 @@ if (!(empty($share_info['content_approved']))):
 	endif;
 
 $form_action_xhr_url = "https://diis.online/?view=share&parameter=". $share_info['share_id'] ."&action=xhr&language=".$language_request;
-echo "<form id='edit-window-form' target='_top' action-xhr='".$form_action_xhr_url."' method='post' custom-validation-reporting='as-you-go' on='invalid:edit-window-form-submission-valid.hide;valid:edit-window-form-submission-invalid.hide'>"; // Use action attribute so input type submit works
+echo "<form id='edit-window-form' target='_top' action-xhr='".$form_action_xhr_url."' method='post' custom-validation-reporting='as-you-go'>";
 
 echo "<div id='edit-window-edit-post-alignment'>";
 
@@ -25,7 +25,7 @@ if (!(empty($share_info['content_approved']))):
 	endif;
 
 // Put identifier here...
-echo "<textarea name='content_draft' placeholder='". $translatable_elements['write-here'][$language_request] ."' id='edit-window-draft-textarea' on='input-debounced:edit-window-form.submit,edit-window-form-submission-alert-empty-state.hide' on='invalid:edit-window-form-submission-notice-alignment.hide;valid:edit-window-form-submission-notice-alignment.show' required>".$share_info['content_draft']."</textarea>";
+echo "<textarea name='content_draft' placeholder='". $translatable_elements['write-here'][$language_request] ."' id='edit-window-draft-textarea' on='input-debounced:edit-window-form.submit,edit-window-form-submission-alert-empty-state.hide;invalid:edit-window-form-submission-notice-alignment.hide;valid:edit-window-form-submission-notice-alignment.show' required>".$share_info['content_draft']."</textarea>";
 
 if (!(empty($share_info['content_approved']))):
 	echo "<button id='edit-window-reset-button' type='reset'><i class='material-icons'>cancel_presentation</i> ". $translatable_elements['undo-changes'][$language_request] ."</button>";
