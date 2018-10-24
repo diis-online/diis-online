7ef783eabf8da824b5b07854c5fc92f8eaed211d
diis-me <41995924+diis-me@users.noreply.github.com>
2018-10-24 09:49:58 +0300

    Update view-share_action-edit.php

--- www/view-share_action-edit.php
+++ www/view-share_action-edit.php
@@ -4,7 +4,7 @@ if (!(empty($share_info['content_approved']))):
 	echo "<amp-lightbox id='edit-window-approved-post-lightbox' layout='nodisplay'>";
 	echo "<span id='edit-window-approved-post-close-button' role='button' tabindex='0' on='tap: edit-window-approved-post-lightbox.close'><i class='material-icons'>cancel</i> Close approved post</span>";
 	echo "<div id='edit-window-approved-post-alignment'>";
-	echo "<span id='edit-window-approved-post-header'>Approved post, live on website</span>";
+	echo "<span id='edit-window-approved-post-header'>". $translatable_elements['approved-post-live-on-website'][$language_request] ."</span>";
 	echo "<hr class='edit-window-stroke'>";
 	echo $share_info['content_approved'];
 	echo "</div>";
@@ -21,17 +21,17 @@ echo "<input type='hidden' name='share_id' value='".$share_info['share_id']."'>"
 echo "<input type='hidden' name='content_status' [value]='content_status_state'>";
 
 if (!(empty($share_info['content_approved']))):
-	echo "<span id='edit-window-approved-post-open-button' role='button' tabindex='0' on='tap: edit-window-approved-post-lightbox.open'><i class='material-icons'>visibility</i> Review approved post</span>";
+	echo "<span id='edit-window-approved-post-open-button' role='button' tabindex='0' on='tap: edit-window-approved-post-lightbox.open'><i class='material-icons'>visibility</i> ". $translatable_elements['review-approved-post'][$language_request] ."</span>";
 	endif;
 
 // Put identifier here...
-echo "<textarea name='content_draft' placeholder='Write here...' id='edit-window-draft-textarea' on='input-debounced:edit-window-form.submit,edit-window-form-submission-alert-empty-state.hide' required>".$share_info['content_draft']."</textarea>";
+echo "<textarea name='content_draft' placeholder='". $translatable_elements['write-here'][$language_request] ."' id='edit-window-draft-textarea' on='input-debounced:edit-window-form.submit,edit-window-form-submission-alert-empty-state.hide' required>".$share_info['content_draft']."</textarea>";
 
 if (!(empty($share_info['content_approved']))):
-	echo "<button id='edit-window-reset-button' type='reset'><i class='material-icons'>cancel_presentation</i> Undo changes</button>";
+	echo "<button id='edit-window-reset-button' type='reset'><i class='material-icons'>cancel_presentation</i> ". $translatable_elements['undo-changes'][$language_request] ."</button>";
 	endif;
 
-// echo "<span id='edit-window-save-button' role='button' tabindex='0' on='tap:edit-window-form-submission-alert-empty-state.hide,edit-window-form.submit'>Save work.</span>";
+// echo "<span id='edit-window-save-button' role='button' tabindex='0' on='tap:edit-window-form-submission-alert-empty-state.hide,edit-window-form.submit'>". $translatable_elements['save-work'][$language_request] ."</span>";
 
 // We need to add something about setting the relationship
 
@@ -39,18 +39,18 @@ echo "</div>";
 
 echo "<div id='edit-window-form-submission-notice-alignment'>";
 echo "<div id='edit-window-form-submission-notice'>";
-	if (empty($share_info['content_draft'])): echo "<span id='edit-window-form-submission-alert-empty-state'>Not saved yet.</span>";
-	else: echo "<span id='edit-window-form-submission-alert-empty-state'>No changes to save.</span>"; endif;
-	echo "<span id='edit-window-form-submission-alert-success' submit-success><template type='amp-mustache'>Saved <amp-timeago id='edit-window-form-submit-timeago' layout='responsive' height='20' width='100' datetime='{{{time}}}' locale='en'>{{{time}}}</amp-timeago>.</template></span>";
-	echo "<span id='edit-window-form-submission-alert-failure' submit-error><template type='amp-mustache'>Not saved! {{{message}}} &nbsp; <span id='edit-window-form-submission-alert-failure-try-again-button' role='button' tabindex='0' on='tap:edit-window-form.submit'>Try again.</span></template></span>";
+	if (empty($share_info['content_draft'])): echo "<span id='edit-window-form-submission-alert-empty-state'>". $translatable_elements['not-saved-yet'][$language_request] ."</span>";
+	else: echo "<span id='edit-window-form-submission-alert-empty-state'>". $translatable_elements['no-changes-to-save'][$language_request] ."</span>"; endif;
+	echo "<span id='edit-window-form-submission-alert-success' submit-success><template type='amp-mustache'>". $translatable_elements['saved'][$language_request] ." <amp-timeago id='edit-window-form-submit-timeago' layout='responsive' height='20' width='100' datetime='{{{time}}}' locale='en'>{{{time}}}</amp-timeago>.</template></span>";
+	echo "<span id='edit-window-form-submission-alert-failure' submit-error><template type='amp-mustache'>". $translatable_elements['not-saved'][$language_request] ." {{{message}}} &nbsp; <span id='edit-window-form-submission-alert-failure-try-again-button' role='button' tabindex='0' on='tap:edit-window-form.submit'>". $translatable_elements['try-again'][$language_request] ."</span></template></span>";
 	echo "<span submitting><template type='amp-mustache'>Syncing...</template></span>";
 echo "</div></div>";
 
 // if (($share_info['author_id'] !== $login_status['user_id']) && (in_array($login_status['level'], ["administrator", "editor"]))):
 if ($share_info['author_id'] == $login_status['user_id']):
-	echo "<div id='edit-window-form-instructions'><p>When you finish writing, submit your work to our editors for review. We will fact-check and proofread and publish it online. If your work is rejected or if we have more questions, we will explain that in the annotations below.</p>";
-	echo "<span id='edit-window-submit-button' role='button' tabindex='0' on='tap:AMP.setState({content_status_state: \"pending\"}),edit-window-form-submission-alert-empty-state.hide,edit-window-form.submit'>Submit for review.</span>";
-	echo "<span id='edit-window-submit-button-caution'>Caution! This cannot be undone.</span></div>";
+	echo "<div id='edit-window-form-instructions'><p>". $translatable_elements['when-you-finish-writing-instructions'][$language_request] ."</p>";
+	echo "<span id='edit-window-submit-button' role='button' tabindex='0' on='tap:AMP.setState({content_status_state: \"pending\"}),edit-window-form-submission-alert-empty-state.hide,edit-window-form.submit'>". $translatable_elements['submit-for-review'][$language_request] ."</span>";
+	echo "<span id='edit-window-submit-button-caution'>". $translatable_elements['caution-this-cannot-be-undone'][$language_request] ."</span></div>";
 	endif;
 
 echo "</form>";
@@ -59,11 +59,11 @@ echo "<div id='edit-window-annotations-alignment'>";
 
 echo "<form target='_top' action-xhr='?view=share&parameter=". $share_info['share_id'] ."&action=updates' method='post'>";
 
-echo "<span id='edit-window-annotations-header'><i class='material-icons'>all_inbox</i> Annotations</span>";
-echo "<button on='tap:edit-window-annotations-list.refresh' id='edit-window-annotations-refresh-button'><i class='material-icons'>refresh</i> Check for updates</button>";
+echo "<span id='edit-window-annotations-header'><i class='material-icons'>all_inbox</i> ". $translatable_elements['annotations'][$language_request] ."</span>";
+echo "<button on='tap:edit-window-annotations-list.refresh' id='edit-window-annotations-refresh-button'><i class='material-icons'>refresh</i> ". $translatable_elements['check-for-updates'][$language_request] ."</button>";
 
 // Put identifier
-echo "<textarea name='body' placeholder='Add annotation...' id='edit-window-annotations-textarea'></textarea>";
+echo "<textarea name='body' placeholder='". $translatable_elements['write-here'][$language_request] ."' id='edit-window-annotations-textarea'></textarea>";
 echo "<input type='submit' id='edit-window-annotations-annotation-button' name='submit_annotation' value='Add annotation'>";
 
 echo "<amp-list id='edit-window-annotations-list' max-items='10' src='https://diis.online/?view=share&parameter=".$share_info['share_id']."&action=updates&language=".$language_request."'>";
