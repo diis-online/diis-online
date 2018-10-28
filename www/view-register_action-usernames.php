4e1e3a074ac38862c4d439b374aef7c31c1f26ab
diis-me <41995924+diis-me@users.noreply.github.com>
2018-10-28 17:13:01 +0300

    Update view-register_action-usernames.php

--- www/view-register_action-usernames.php
+++ www/view-register_action-usernames.php
@@ -63,9 +63,9 @@ while ($count_temp < 5):
 						   
 	$json_result['items'][] = [
 		"combined" => implode("-", $combined_temp),
-		"username-one" => $adjective_one_temp, 
-		"username-two" => $adjective_two_temp, 
-		"username-three" => $noun_temp,
+		"username-one" => ucfirst($adjective_one_temp), 
+		"username-two" => ucfirst($adjective_two_temp), 
+		"username-three" => ucfirst($noun_temp),
 		];
 
 	$count_temp++; endwhile;
