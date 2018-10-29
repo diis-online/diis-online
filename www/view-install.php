6c9928820e5d3c643591022a089277ecdf95c55b
diis-me <41995924+diis-me@users.noreply.github.com>
2018-10-29 16:02:20 +0300

    Update view-install.php

--- www/view-install.php
+++ www/view-install.php
@@ -205,22 +205,20 @@ elseif ($admin_temp !== 1):
 
 	echo "<p>Your author name is also your username for sign-in. It is automatically generated for privacy and anonymity.</p>";
 
-	echo "<amp-list id='username-option-list' max-items='1' width='auto' height='120' layout='fixed-height' reset-on-refresh='always' src='https://diis.online/?view=register&action=usernames&language=". $language_request ."'>";
-
 	echo "<span class='username-option-helper'>Either accept this automatically generated name,</span>";
 
+	echo "<amp-list id='username-option-list' max-items='1' width='auto' height='120' layout='fixed-height' reset-on-refresh='always' src='https://diis.online/?view=register&action=usernames&language=". $language_request ."'>";
 	echo "<span id='username-option-placeholder' placeholder>". $translatable_elements['loading'][$language_request] ."</span>";
 	echo "<span id='username-option-fallback' fallback>". $translatable_elements['failed-to-load-options'][$language_request] ."</span>";
 	echo "<template type='amp-mustache'>";
 		echo "<input class='username-options-list-item-input' name='username' value='{{combined}}' type='hidden'>";
 		echo "<span id='username-option-show'>{{username-one}} {{username-two}} {{username-three}}</span>";
 	echo "</template>";
+	echo "</amp-list>";
 
 	// Or choose to get the newest one
 	echo "<span class='username-option-helper'>Or choose to generate a new name,</span>";
 	echo "<span tabindex='0' role='button' on='tap:username-options-list.refresh' id='username-option-new-button'><i class='material-icons'>refresh</i> ". $translatable_elements['generate-new-name'][$language_request] ."</span>";
-
-	echo "</amp-list>";
 
 	// Now let the user go on to the next step
 
