ca3fda45bd12439a2c177c905c8222b580c05c12
diis-me <41995924+diis-me@users.noreply.github.com>
2018-10-30 17:31:39 +0300

    Update view-register.php

--- www/view-register.php
+++ www/view-register.php
@@ -60,7 +60,7 @@ if ($parameter_request == "administrator"):
 
 	endif;
 
-echo "<input type='submit' name='submit' value='Create administrator.'>";
+echo "<span id='register-window-create-button' role='button' tabindex='0'>Create account</span>";
 
 echo "<div submit-success><template type='amp-mustache'>Success! {{{message}}}</template></div>";
 echo "<div submit-error><template type='amp-mustache'>{{{message}}}</template></div>";
