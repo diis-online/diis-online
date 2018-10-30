bc273d05e9260da2a086642519f6ddfbc38038ef
diis-me <41995924+diis-me@users.noreply.github.com>
2018-10-30 21:25:36 +0300

    Update view-register.php

--- www/view-register.php
+++ www/view-register.php
@@ -61,8 +61,7 @@ if ($parameter_request == "administrator"):
 	echo "<span id='register-window-security-key'>DSK JLN SDF J32 343</span>";
 	
 	// And some recovery codes...
-	echo "<p>Save these recovery codes.</p>";
-	echo "<span class='register-window-helper'>These one-time codes can be used if your phone is lost.</span>";
+	echo "<p>Save these recovery codes. They can each be used once in case your phone is lost.</span>";
 	echo "<span id='register-window-recovery-keys'>231 9R8<br>PND 9X5<br>13K 94L</span>";
 
 	echo "</div>";
