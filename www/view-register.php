1205293ed65b4ded8d00a698d83a5352f8c3418b
diis-me <41995924+diis-me@users.noreply.github.com>
2018-10-30 17:36:44 +0300

    Update view-register.php

--- www/view-register.php
+++ www/view-register.php
@@ -47,8 +47,8 @@ if ($parameter_request == "administrator"):
 
 	// Install the app onto your phone...
 	echo "<p>1) Install Google Authenticator on your phone,</p>";
-	echo "<a href='https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2'>Android</a>";
-	echo "<a href='https://itunes.apple.com/us/app/google-authenticator/id388497605'>iOS</a>";
+	echo "<a href='https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2'><span id='register-window-download-link'>Android</span></a>";
+	echo "<a href='https://itunes.apple.com/us/app/google-authenticator/id388497605'><span id='register-window-download-link'>iOS</span></a>";
 
 	// The user will add a lengthy key to Authenticator...
 	echo "<p>2) Add this key to the app,</p>";
@@ -57,6 +57,10 @@ if ($parameter_request == "administrator"):
 	// The user will input their authentication code to check it...
 	echo "<p>3) Check the six-digit code below,</p>"; 
 	echo "<input id='authenticator-input' type='number' pattern='.{6,6}' max='999999' name='pin-authenticator'>";
+
+	// And some recovery codes...
+	echo "<p>4) Save these recovery codes in case you lose your phone,</p>";
+	// Some recovery codes...
 
 	endif;
 
