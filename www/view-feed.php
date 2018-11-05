fc60ed8f7d42c4639d59c2352e1bd55e99aa7b1c
diis-me <41995924+diis-me@users.noreply.github.com>
2018-11-05 18:11:25 +0300

    Update view-feed.php

--- www/view-feed.php
+++ www/view-feed.php
@@ -13,7 +13,7 @@ echo "<p>".$translatable_elements['who-we-are'][$language_request]."</p>";
 if (empty($login_status)):
 	echo "<h2 amp-fx='parallax' data-parallax-factor='1.05'>".$translatable_elements['become-a-publisher'][$language_request]."</h2>";
 	echo "<p amp-fx='parallax' data-parallax-factor='1.03'>".$translatable_elements['publisher-join-instructions'][$language_request]."</p>";
-	echo "<a href='?view=register' amp-fx='parallax' data-parallax-factor='0.98'><span id='home-window-register-button'>".$translatable_elements['create-account'][$language_request]."</span></a>";
+	echo "<a href='?view=register' amp-fx='parallax' data-parallax-factor='0.98'><span id='home-window-create-button'>".$translatable_elements['create-account'][$language_request]."</span></a>";
 	endif;
 
 echo "<h2>".$translatable_elements['safety-first'][$language_request]."</h2>";
