6984d43a92d28252917d7c83e606edec0125d539
diis-me <41995924+diis-me@users.noreply.github.com>
2018-11-06 17:32:07 +0300

    Update view-feed.php

--- www/view-feed.php
+++ www/view-feed.php
@@ -3,28 +3,52 @@
 // First, they have to pick a pseudonym
 echo "<div id='feed-window-shares-alignment'>";
 
+	echo "<amp-state id='feedmore' src='https://ampbyexample.com/json/related_products.json'></amp-state>"
+//	echo "<amp-state id='feedmore' src='/json/related_products.json'></amp-state>"
+
+	// So this initializes feedmore with empty values
+	echo "<amp-state id='feedpaging'><script type='application/json'>";
+	echo '{ "feedpagingindex": 1, "feedpagingnext": true }';
+	echo "</script></amp-state>";
+
+	// This will totally refresh the feed
 	echo "<span id='feed-window-refresh-button' role='button' tabindex='0' on='tap:feed-window-shares.refresh'><i class='material-icons'>refresh</i> ". $translatable_elements['refresh-shares'][$language_request] ."</span>";
 
-	echo "<amp-list id='feed-window-shares' max-items='5' height='900' [height]='productsState.items.length * 24' layout='fixed-height' src='https://diis.online/?view=feed&action=updates&language=". $language_request ."' [src]="productsState.items">";
+	// This is the feed itself
+	$html_temp = [
+		"id"		=> "feed-window-shares",
+		"height"	=> "900",
+		"[height]"	=> "feedmore.items.length * 40",
+		"src"		=> "http://ampbyexample.com/json/related_products.json",
+//		"src"		=> "https://diis.online/?view=feed&action=xhr&language=". $language_request,
+		"[src]		=> "feedmore.items",
+		];
+	echo "<amp-list ". html_implode($html_temp) .">";
 	echo "<span id='feed-window-shares-fallback' fallback>". $translatable_elements['failed-to-load'][$language_request] ."</span>";
 	echo "<template type='amp-mustache'><div id='feed-window-share'>";
-		echo "{{name}}";
+		echo "{{img}}";
 		echo "<br>";
-		echo "{{body}}";
+		echo "{{name}}";
 		echo "<br><br>";
 	echo "</div></template></amp-list>";
 
-	echo "<form method='GET' action='/json/more_related_products_page' action-xhr='/json/more_related_products_page' target='_top' on='";
-	echo "submit-success: AMP.setState({
-            productsState: {
-              items: productsState.items.concat(event.response.items)
-            },
-            product: {
-              moreItemsPageIndex: product.moreItemsPageIndex + 1,
-              hasMorePages: event.response.hasMorePages
-            }
-          });";
-	echo "'>";
+	$amp_setstate_temp = [
+		"feedmore" => [
+			"items" => "feedmore.items.concat(event.response.items)" ],
+		"feedpaging" => [
+			"moreItemsPageIndex" => "feedpaging.moreItemsPageIndex + 1",
+			"hasMorePages" => "event.response.hasMorePages" ],
+//			"feedpagingindex" => "feedmore.items.concat(event.response.items)",
+//			"feedpagingnext" => "event.response.feedpaginnext" ],
+		];
+	$html_temp = [
+		"method"	=> "get",
+		"action-xhr"	=> "https://ampbyexample.com/json/more_related_products_page",
+//		"action-xhr"	=> "https://diis.online/",
+		"target"	=> "_top",
+		"on"		=> "submit-success: AMP.setState({". json_encode($amp_setstate_temp) ."});",
+		];
+	echo "<form ". html_implode($html_temp) .">
 	echo "<input type='hidden' name='moreItemsPageIndex' value='0' [value]='product.moreItemsPageIndex'>";
 	echo "<input type='submit' value='Show more'>";
 	echo "</form>";
