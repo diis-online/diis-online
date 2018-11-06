964b1da434ee314991a39dd0fb2e0bb67a228a41
diis-me <41995924+diis-me@users.noreply.github.com>
2018-11-06 20:57:10 +0300

    Update view-feed.php

--- www/view-feed.php
+++ www/view-feed.php
@@ -34,9 +34,10 @@ echo "<div id='feed-window-shares-alignment'>";
 		'feedpaging':  { moreItemsPageIndex: feedpaging.moreItemsPageIndex + 1, hasMorePages: event.response.hasMorePages } }";
 	$html_temp = [
 		"id"		=> "feed-window-form",
-		"method"	=> "get",
-//		"method"	=> "post",
-		"action-xhr"	=> "https://diis.online/?view=feed&action=updates&language=". $language_request,
+//		"method"	=> "get",
+		"method"	=> "post",
+//		"action-xhr"	=> "https://diis.online/?view=feed&action=updates&language=". $language_request,
+		"action-xhr"	=> "https://ampbyexample.com/json/related_products.json?moreItemsPageIndex=1",
 		"target"	=> "_top",
 		"on"		=> "submit-success: AMP.setState(".$amp_setstate_temp.");",
 		];
