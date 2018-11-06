6ab7b7b653ca69fb7e9b3a0e61080efd2cacf050
diis-me <41995924+diis-me@users.noreply.github.com>
2018-11-06 20:59:01 +0300

    Update view-feed_action-updates.php

--- www/view-feed_action-updates.php
+++ www/view-feed_action-updates.php
@@ -1,3 +1,65 @@
+<? if (!(empty($_POST))): ?>
+
+{
+  "items": [{
+    "id": 1,
+    "img": "/img/product1_640x426.jpg",
+    "name": "dfdfg",
+    "price": "1.99",
+    "stars": "&#9733;&#9733;&#9733;&#9733;&#9733;",
+    "attribution": "visualhunt",
+    "url": "#",
+    "color": "green"
+  }, {
+    "id": 2,
+    "img": "/img/product2_640x426.jpg",
+    "name": "dfgdfgg",
+    "attribution": "visualhunt",
+    "price": "0.99",
+    "stars": "&#9733;&#9733;&#9733;&#9733;&#9734;",
+    "url": "#",
+    "color": "orange"
+  }, {
+    "id": 3,
+    "img": "/img/product3_640x426.jpg",
+    "name": "dfgdfg",
+    "attribution": "visualhunt",
+    "price": "1.50",
+    "stars": "&#9733;&#9733;&#9733;&#9734;&#9734;",
+    "url": "#",
+    "color": "green"
+  }, {
+    "id": 4,
+    "img": "/img/product4_640x426.jpg",
+    "name": "dfgdfgdg",
+    "attribution": "pixabay",
+    "price": "1.50",
+    "stars": "&#9733;&#9733;&#9733;&#9733;&#9733;",
+    "url": "#",
+    "color": "yellow"
+  }, {
+    "id": 5,
+    "img": "/img/product5_640x408.jpg",
+    "name": "Watdfgdfgddfgermelon",
+    "attribution": "pixabay",
+    "price": "4.50",
+    "stars": "&#9733;&#9733;&#9733;&#9733;&#9733;",
+    "url": "#",
+    "color": "red"
+  }, {
+    "id": 6,
+    "img": "/img/product6_640x424.jpg",
+    "name": "Meldfgdfgdfgdfgdfgon",
+    "attribution": "pixabay",
+    "price": "3.50",
+    "stars": "&#9733;&#9733;&#9733;&#9733;&#9733;",
+    "url": "#",
+    "color": "yellow"
+  }]
+}
+
+<? exit; endif; ?>
+
 {
   "items": [{
     "id": 1,
