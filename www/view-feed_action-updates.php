e27cb9b0355089d77cd5f0d3fdb1c53fb0cdad43
diis-me <41995924+diis-me@users.noreply.github.com>
2018-11-06 12:52:59 +0300

    Update view-feed_action-updates.php

--- www/view-feed_action-updates.php
+++ www/view-feed_action-updates.php
@@ -1,3 +1,15 @@
 <? // Simple API for latest in feed... 
 
+$json_result['items'][] = [
+		"name" => "Article 1",
+		"body" => "sdfgsdglfkdnjfg sdkflg dlskjfg sfgsdjlfg sdfgklsdfjgsd sdfkg sdkflg dskf g sdfgklsdfgk dskfg dsfkgdfkg dfkg sdkfg sdfklgj . dsfklg ds",
+		];
+
+$json_result['items'][] = [
+		"name" => "Article 2",
+		"body" => "sdfgfgertw fgg mdsflkgoert werotpert ewrtk dldgf; sdfkgdfg sdfgkj dfg 3ert.",
+		];
+
+echo json_encode($json_result);
+
 ?>
