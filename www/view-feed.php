16b066fcf4abe11ed4a6a81326d90eaa382593d1
diis-me <41995924+diis-me@users.noreply.github.com>
2018-11-06 13:31:49 +0300

    Update view-feed.php

--- www/view-feed.php
+++ www/view-feed.php
@@ -1,6 +1,18 @@
 <? if (empty($script_code)): exit; endif;
 
-echo "<span id='navigation-chooser-feed-button' amp-fx='parallax' data-parallax-factor='1.4'><i class='material-icons'>refresh</i> ". $translatable_elements['refresh-shares'][$language_request] ."</span>";
+echo "<span id='feed-window-refresh-button' role='button' tabindex='0' on='tap:feed-window-shares.refresh'><i class='material-icons'>refresh</i> ". $translatable_elements['refresh-shares'][$language_request] ."</span>";
+
+// First, they have to pick a pseudonym
+echo "<div id='feed-window-shares-alignment'>";
+	echo "<amp-list id='feed-window-shares' max-items='5' height='900' layout='fixed-height' src='https://diis.online/?view=feed&action=updates&language=". $language_request ."'>";
+	echo "<span id='feed-window-shares-fallback' fallback>". $translatable_elements['failed-to-load'][$language_request] ."</span>";
+	echo "<template type='amp-mustache'><div id='feed-window-share'>";
+		echo "{{name}}";
+		echo "<br>";
+		echo "{{body}}";
+		echo "<br><br>";
+	echo "</div></template></amp-list>";
+	echo "</div>";
 
 echo "<div id='feed-window-mission-alignment' amp-fx='fade-in' data-easing='linear' data-margin-start='2%' data-duration='1000ms'>";
 	echo "<h2 amp-fx='parallax' data-parallax-factor='1.05'>".$translatable_elements['coming-soon'][$language_request]."</h2>";
