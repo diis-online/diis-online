bcf7e8b9cec913d3f0279d8571834a21b51f9834
diis-me <41995924+diis-me@users.noreply.github.com>
2018-11-06 20:22:29 +0300

    Update view-feed_action-updates.php

--- www/view-feed_action-updates.php
+++ www/view-feed_action-updates.php
@@ -2,17 +2,6 @@
 
 $count_temp = random_number(1);
 
-if (!(empty($_POST)) || !(empty($_REQUEST))):
-
-	$_POST['page'] = $_POST['page'] ?? $_REQUEST['page'] ?? random_number(3);
-
-	$count_temp = $_POST['page'] + 2;
-
-	$json_result['next'] = "true";
-	$json_result['page'] = $_POST['page']+1;
-
-	endif;
-
 $counter_temp = 0;
 
 while ($counter_temp < 5):
@@ -24,6 +13,18 @@ while ($counter_temp < 5):
 		"body" => random_number(10)." ".random_number(8)." ".random_number(3)." ".random_number(5),
 		];
 	endwhile;
+
+
+if (!(empty($_POST)) || !(empty($_REQUEST))):
+
+	$_POST['page'] = $_POST['page'] ?? $_REQUEST['page'] ?? random_number(3);
+
+	$count_temp = $_POST['page'] + 2;
+
+	$json_result['next'] = "true";
+	$json_result['page'] = $_POST['page']+1;
+
+	endif;
 
 echo json_encode($json_result);
 
