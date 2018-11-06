eeb26d6c13b41616a1e07c3ecce38effa7e33c05
diis-me <41995924+diis-me@users.noreply.github.com>
2018-11-06 13:31:43 +0300

    Update view-feed_action-updates.php

--- www/view-feed_action-updates.php
+++ www/view-feed_action-updates.php
@@ -2,12 +2,22 @@
 
 $json_result['items'][] = [
 		"name" => "Article 1",
-		"body" => "sdfgsdglfkdnjfg sdkflg dlskjfg sfgsdjlfg sdfgklsdfjgsd sdfkg sdkflg dskf g sdfgklsdfgk dskfg dsfkgdfkg dfkg sdkfg sdfklgj . dsfklg ds",
+		"body" => random_number(10)." ".random_number(8)." ".random_number(3)." ".random_number(5),
 		];
 
 $json_result['items'][] = [
 		"name" => "Article 2",
-		"body" => "sdfgfgertw fgg mdsflkgoert werotpert ewrtk dldgf; sdfkgdfg sdfgkj dfg 3ert.",
+		"body" => random_number(10)." ".random_number(8)." ".random_number(3)." ".random_number(5),
+		];
+
+$json_result['items'][] = [
+		"name" => "Article 3",
+		"body" => random_number(10)." ".random_number(8)." ".random_number(3)." ".random_number(5),
+		];
+
+$json_result['items'][] = [
+		"name" => "Article 4",
+		"body" => random_number(10)." ".random_number(8)." ".random_number(3)." ".random_number(5),
 		];
 
 echo json_encode($json_result);
