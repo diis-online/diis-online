ac2d4f22f92bd71b54de87ef31026f515764ea23
diis-me <41995924+diis-me@users.noreply.github.com>
2018-11-06 12:49:21 +0300

    Update view-feed.php

--- www/view-feed.php
+++ www/view-feed.php
@@ -2,18 +2,18 @@
 
 echo "<span id='navigation-chooser-feed-button' amp-fx='parallax' data-parallax-factor='1.4'><i class='material-icons'>refresh</i> ". $translatable_elements['refresh-shares'][$language_request] ."</span>";
 
-echo "<div id='feed-window-mission-alignment'>";
+echo "<div id='feed-window-mission-alignment' amp-fx='fade-in' data-easing='linear' data-margin-start='2%' data-duration='800ms'>";
 	echo "<h2 amp-fx='parallax' data-parallax-factor='1.05'>".$translatable_elements['coming-soon'][$language_request]."</h2>";
 	echo "<p amp-fx='parallax' data-parallax-factor='1.05'>".$translatable_elements['mission-statement'][$language_request]."</p>";
 	echo "</div>";
 
-echo "<div id='feed-window-who-we-are-alignment' amp-fx='fade-in' data-easing='ease-in' data-margin-start='2%' data-duration='1200ms'>";
+echo "<div id='feed-window-who-we-are-alignment' amp-fx='fade-in' data-easing='linear' data-margin-start='2%' data-duration='1200ms'>";
 	echo "<h2 amp-fx='parallax' data-parallax-factor='1.03'>".$translatable_elements['unsilenced'][$language_request]."</h2>";
 	echo "<p amp-fx='parallax' data-parallax-factor='1.03'>".$translatable_elements['who-we-are'][$language_request]."</p>";
 	echo "</div>";
 
 if (empty($login_status)):
-	echo "<div id='feed-window-become-a-publisher-alignment' amp-fx='fade-in' data-easing='ease-in' data-margin-start='2%' data-duration='1200ms'>";
+	echo "<div id='feed-window-become-a-publisher-alignment' amp-fx='fade-in' data-easing='ease-in' data-margin-start='2%' data-duration='1000ms'>";
 		echo "<h2>".$translatable_elements['become-a-publisher'][$language_request]."</h2>";
 		echo "<p>".$translatable_elements['publisher-join-instructions'][$language_request]."</p>";
 		echo "<a href='?view=register' amp-fx='parallax' data-parallax-factor='0.98'><span id='feed-window-create-button'>".$translatable_elements['create-account'][$language_request]."</span></a>";
