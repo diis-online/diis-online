594f10b72c3a771844575d27d793cfcd27d31b06
diis-me <41995924+diis-me@users.noreply.github.com>
2018-11-07 00:49:56 +0300

    Update view-feed.php

--- www/view-feed.php
+++ www/view-feed.php
@@ -40,8 +40,8 @@ echo "<div id='feed-window-shares-alignment'>";
 		"width"		=> "800",
 		"height"	=> "570px",
 		"[height]"	=> "ceil(feedcontent.items.length/2) * 190 + 'px'",
-		"heights"	=> "(min-width:700 px) 570px, 950px",
-		"[heights]"	=> "'(min-width:700 px) '+ceil(feedcontent.items.length/2)*190+'px, '+ feedcontent.items.length*190+'px'",
+		"heights"	=> "(min-width: 700px) 570px, 950px",
+		"[heights]"	=> "'(min-width: 700px) '+ceil(feedcontent.items.length/2)*190+'px, '+ feedcontent.items.length*190+'px'",
 		"src"		=> "https://diis.online/?view=feed&action=updates&language=". $language_request,
 		"[src]"		=> "feedcontent.items",
 		];
