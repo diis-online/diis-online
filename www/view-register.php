78ca17b35efa4b1d5984d44b2c49ac75da381ef7
diis-me <41995924+diis-me@users.noreply.github.com>
2018-11-07 18:38:18 +0300

    Update view-register.php

--- www/view-register.php
+++ www/view-register.php
@@ -60,13 +60,13 @@ if ($parameter_request == "administrator"):
 	echo "<a href='https://itunes.apple.com/us/app/google-authenticator/id388497605' target='_blank'><span class='register-window-install-link'>". $translatable_elements['install-on-ios'][$language_request] ."</span></a>";
 
 	// Sync by a link that opens to the app...
-	$security_key = random_thirtytwo(16);
-//	$security_key = "QLTVZG73VDVF3ZHR";
-	echo "<br><br><span class='register-window-helper'>". $translatable_elements['add-your-security-key'][$language_request] ."</span>";
-	echo "<span id='register-window-security-key'>". chunk_split($security_key, 4, ' ') ."</span>";
-	echo "<a href='otpauth://totp/My account?secret=". encode_thirtytwo($security_key) ."&issuer=Diis'><span class='register-window-security-key-link'>". $translatable_elements['reading-this-on-your-phone'][$language_request] ."</span>"; // This link can also be sent to a QR code
+	$authenticator_key = random_thirtytwo(16);
+//	$authenticator_key = "QLTVZG73VDVF3ZHR";
+	echo "<br><br><span class='register-window-helper'>". $translatable_elements['add-your-authenticator-key'][$language_request] ."</span>";
+	echo "<span id='register-window-security-key'>". chunk_split($authenticator_key, 4, ' ') ."</span>";
+	echo "<a href='otpauth://totp/My account?secret=". encode_thirtytwo($authenticator_key) ."&issuer=Diis'><span class='register-window-security-key-link'>". $translatable_elements['reading-this-on-your-phone'][$language_request] ."</span>"; // This link can also be sent to a QR code
 	echo "<span class='register-window-security-key-link'><i class='material-icons'>launch</i> ". $translatable_elements['tap-here-to-add-your-security-key-automatically'][$language_request] ."</span></a>";
-	echo "<input type='hidden' name='security_key' value='". $security_key ."'>";
+	echo "<input type='hidden' name='authenticator_key' value='". $authenticator_key ."'>";
 
 	echo "</div>";
 
