b529b68d939288222627a7727a46004b64535a37
diis-me <41995924+diis-me@users.noreply.github.com>
2018-11-08 13:08:39 +0300

    Update configuration-sample.php

--- www/configuration-sample.php
+++ www/configuration-sample.php
@@ -6,6 +6,6 @@ $postgres_user = "";
 $postgres_password = "";
 
 // Change this to "enabled" and go to https://diis.online/?view=install
-// This will install the first time and allow you to create an administrator
+// Diis will set up its database requirements and allow you to create an administrator
 // Once you have finished that that then change this back to "disabled"
 $allow_install = "disabled"; ?>
