cf4fca2e2538d3d7a904c3c74d54048d1494b822
diis-me <41995924+diis-me@users.noreply.github.com>
2018-11-08 16:27:53 +0300

    Update view-signin_action-xhr.php

--- www/view-signin_action-xhr.php
+++ www/view-signin_action-xhr.php
@@ -1,8 +1,13 @@
-<?
+<? if (empty($script_code)): exit; endif;
+
+header("Content-type: application/json");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Origin: https://diis.online");
+header("AMP-Access-Control-Allow-Source-Origin: https://diis.online");
 
 // Check posted information
 
-// Then process it and make cookies
+// Then process it and eithe reject it or sign in the user, update the session database, and make cookies
 
 // And give the appropriate response, or redirect
 
