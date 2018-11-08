4114b85ac05bb716a04ee533d45618827eb0d3e0
diis-me <41995924+diis-me@users.noreply.github.com>
2018-11-08 16:27:59 +0300

    Update view-account.php

--- www/view-account.php
+++ www/view-account.php
@@ -1,3 +1,15 @@
 <? // Don't have 2FA yet? Learn how to set it up and unlock more account features.
 
+// If you have 2FA then enter it to unlock your features
+
+// Show your shares....
+
+// Info about your account like previous logins and current session... (deactivate other sessions)...
+
+// Show your username in different languages, you can use these to log in too
+
+// Reset your passcode
+
+// Resert your 2FA
+
 ?>
