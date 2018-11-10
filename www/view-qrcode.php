4fa004a7a353da207521e8c87ce86d42e9bd7f5f
diis-me <41995924+diis-me@users.noreply.github.com>
2018-11-10 15:22:06 +0300

    Update view-qrcode.php

--- www/view-qrcode.php
+++ www/view-qrcode.php
@@ -1,28 +1,33 @@
 <? if (empty($script_code)): exit; endif; ?>
 
 <?xml version="1.0" standalone="yes"?>
+
 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-50 0 200 100">
+
 <g id="qrcode"/>
+
 <foreignObject x="-50" y="0" width="100" height="100">
+
 <body xmlns="http://www.w3.org/1999/xhtml" style="padding:0; margin:0">
-<script type="application/ecmascript" src="qrcode.min.js"></script>
+	
+<script type="application/ecmascript" src="view-qrcode.js"></script>
+
 <script type="application/ecmascript">
 	var elem = document.getElementById("qrcode");
 	var qrcode = new QRCode(elem, {
     		width : 100,
    		height : 100
 		});
-
-function makeCode () {
-	var elText = "<? echo $parameter_request ?>";
-	qrcode.makeCode(elText);
-	}
-
-makeCode();
-
-document.getElementById("text").onkeyup = function (e) { makeCode(); };
-</script>
+	function makeCode () {
+		var elText = "<? echo $parameter_request ?>";
+		qrcode.makeCode(elText);
+		}
+	makeCode();
+	document.getElementById("text").onkeyup = function (e) { makeCode(); };
+	</script>
 
 </body>
+	
 </foreignObject>
+
 </svg>
