78cd360fd58f77f560e8102cf11eb6ce3488602a
diis-me <41995924+diis-me@users.noreply.github.com>
2018-11-10 17:37:06 +0300

    Update view-qrcode.php

--- www/view-qrcode.php
+++ www/view-qrcode.php
@@ -3,9 +3,9 @@
 header("Content-type: image/svg+xml");
 
 echo '<?xml version="1.0" standalone="yes"?>
-<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-50 0 200 100">
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
 <g id="qrcode"/>
-<foreignObject x="-50" y="0" width="100" height="100">
+<foreignObject width="100" height="100">
 
 <body xmlns="http://www.w3.org/1999/xhtml" style="padding:0; margin:0">
 
