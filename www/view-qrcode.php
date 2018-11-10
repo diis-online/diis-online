cfa84cfcdbd15b48dc67843da6bb12285ce52848
diis-me <41995924+diis-me@users.noreply.github.com>
2018-11-10 16:11:58 +0300

    Update view-qrcode.php

--- www/view-qrcode.php
+++ www/view-qrcode.php
@@ -8,7 +8,7 @@ echo '<?xml version="1.0" standalone="yes"?>
 <body xmlns="http://www.w3.org/1999/xhtml" style="padding:0; margin:0">
 
 <!-- From https://davidshimjs.github.io/qrcodejs/ -->
-<script type="application/ecmascript" src="/view-qrcode.js"></script>
+<script type="application/ecmascript" src="view-qrcode.js"></script>
   
 <script type="application/ecmascript">
 	var elem = document.getElementById("qrcode");
