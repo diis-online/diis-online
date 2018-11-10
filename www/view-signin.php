c635b7bf23960c25b845c7a8266dcf4a8a29564d
diis-me <41995924+diis-me@users.noreply.github.com>
2018-11-10 18:01:14 +0300

    Update view-signin.php

--- www/view-signin.php
+++ www/view-signin.php
@@ -10,13 +10,13 @@ echo "<p>Not a publisher yet? <a href='https://diis.online/?view=register'>". $t
 echo "<form id='signin-window-form' method='post' action-xhr='https://diis.online?view=signin&action=xhr&language=". $language_request ."'>";
 
 echo "<span class='signin-window-helper'>Enter your name.</span>";
-echo "<input id='signin-window-name-input' type='text' name='name'>";
+echo "<input id='signin-window-name-input' type='text' name='name' required>";
 
 // Later on, allow the form to receive a recommended spelling
 // And display a 'Did you mean...' button which updates the name input when you press it
 
 echo "<span class='signin-window-helper'>Enter your passcode.</span>";
-echo "<input id='signin-window-passcode-input' type='password' name='passcode'>";
+echo "<input id='signin-window-passcode-input' type='password' name='passcode' required>";
 
 echo "<span id='signin-window-signin-button' role='button' tabindex='0' on-'tap:signin-window-form.submit'>". $translatable_elements['sign-in'][$language_request] .".</span>";
 
