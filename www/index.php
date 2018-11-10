db1819a385bf198f6ba8f6ce18bfc269ff3b060d
diis-me <41995924+diis-me@users.noreply.github.com>
2018-11-10 17:16:24 +0300

    Update index.php

--- www/index.php
+++ www/index.php
@@ -367,7 +367,7 @@ function html_implode($array_temp) {
 
 // If there is no cookie, then show the info
 
-if ( ($view_request == "qrcode") || ($_SERVER['REQUEST_URI'] == "qrcode.svg")):
+if ( ($view_request == "qrcode") || ($_SERVER['REQUEST_URI'] == "/qrcode.svg")):
 	if (empty($parameter_request)): exit; endif;
 	include_once("view-qrcode.php");
 	exit; endif;
