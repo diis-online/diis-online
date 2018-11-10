d1e5b19978b6e598f0d7b6c654b749302ae03740
diis-me <41995924+diis-me@users.noreply.github.com>
2018-11-10 15:30:23 +0300

    Update view-qrcode.php

--- www/view-qrcode.php
+++ www/view-qrcode.php
@@ -1,17 +1,14 @@
 <? if (empty($script_code)): exit; endif; ?>
 
-echo '<?xml version="1.0" standalone="yes"?>';
+<?xml version="1.0" standalone="yes"?>
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-50 0 200 100">
+<g id="qrcode"/>
+<foreignObject x="-50" y="0" width="100" height="100">
 
-echo '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-50 0 200 100">';
+<body xmlns="http://www.w3.org/1999/xhtml" style="padding:0; margin:0">
 
-echo '<g id="qrcode"/>';
-
-echo '<foreignObject x="-50" y="0" width="100" height="100">';
-
-echo '<body xmlns="http://www.w3.org/1999/xhtml" style="padding:0; margin:0">';
-	
-// From https://davidshimjs.github.io/qrcodejs/
-echo '<script type="application/ecmascript" src="view-qrcode.js"></script>'; ?>
+<!-- From https://davidshimjs.github.io/qrcodejs/ -->
+<script type="application/ecmascript" src="view-qrcode.js"></script>
   
 <script type="application/ecmascript">
 	var elem = document.getElementById("qrcode");
@@ -27,8 +24,7 @@ echo '<script type="application/ecmascript" src="view-qrcode.js"></script>'; ?>
 	document.getElementById("text").onkeyup = function (e) { makeCode(); };
 	</script>
 
-<? echo '</body>';
+</body>
 
-echo '</foreignObject>';
-
-echo '</svg>'; ?>
+</foreignObject>
+</svg>
