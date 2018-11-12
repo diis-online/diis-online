da07cdd6687686e6da262bc00cedd672f26d55eb
diis-me <41995924+diis-me@users.noreply.github.com>
2018-11-12 13:45:48 +0300

    Update view-signin_action-xhr.php

--- www/view-signin_action-xhr.php
+++ www/view-signin_action-xhr.php
@@ -12,33 +12,41 @@ if (strlen($_POST['name']) > 40): json_output("failure", "Name too long."); endi
 if (strlen($_POST['name']) < 9): json_output("failure", "Name too short."); endif;
 if (empty($_POST['passcode'])): json_output("failure", "Passcode was empty."); endif;
 
-$name_array = explode(" ", $_POST['name']);
-foreach ($name_array as $key_temp => $name_temp):
-  if (	ctype_space($name_temp) || 
-				empty($name_temp) || 
-				(strlen($name_temp) < 2) || 
-				($name_temp == "and") ):
-		unset($name_array[$key_temp]);
-		continue; endif;
-	$name_array[$key_temp] = str_replace([".", ","], null, $name_temp);
-  endforeach;
+$name_array_temp = explode(" ", $_POST['name']);
+$name_array = [];
+
+// Remove whitespace
+foreach ($name_array_temp as $key_temp => $name_temp):
+	if (ctype_space($name_temp)): continue; endif;
+	if (empty($name_temp)): continue; endif;
+	if (strlen($name_temp) < 2): continue; endif;
+	if ($name_temp == "and"): continue; endif;
+	$name_array[] = str_replace([".", ","], null, $name_temp);
+ 	endforeach;
 if (count($name_array) < 3): json_output("failure", "Name too short."); endif;
 if (count($name_array) > 3): json_output("failure", "Name too long."); endif;
 
-
 // Identify the name...
+$statement_temp = "SELECT * FROM username_options";
+$result = pg_query($database_connection, $statement_temp);
+while ($row = pg_fetch_assoc($result)):
+	// If it matches something exactly that's great
+	// If it only matches partially then let's store those for recommendations
+	endwhile;
 
+// Give the recommendation if no exact match
 
+// Write a function that checks the username exactly
 
-// Check passcode against name...
+function username_check($name_one, $name_two, $name_three, $input_name) {
+	
+}
 
+// If an exact match then check passcode against name...
 
-
-// If signin failed, then just output a failure...
-
+// If signin failed because no username, then just output a passcode failure...
+// If signin failed because passcode did not match, then just output a passcode failure...
 
 // If signin passed, then update the session database and make cookies and give a positive response and redirect...
-
-
 
 ?>
