82fb82b63b903497471123ed80c934b358fb1478
diis-me <41995924+diis-me@users.noreply.github.com>
2018-11-12 13:20:15 +0300

    Update view-signin_action-xhr.php

--- www/view-signin_action-xhr.php
+++ www/view-signin_action-xhr.php
@@ -5,10 +5,40 @@ header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Origin: https://diis.online");
 header("AMP-Access-Control-Allow-Source-Origin: https://diis.online");
 
-// Check posted information
+$_POST['name'] = trim($_POST['name']) ?? null;
+$_POST['passcode'] = trim($_POST['passcode']) ?? null;
+if (empty($_POST['name'])): json_output("failure", "Name was empty."); endif;
+if (strlen($_POST['name']) > 40): json_output("failure", "Name too long."); endif;
+if (strlen($_POST['name']) < 9): json_output("failure", "Name too short."); endif;
+if (empty($_POST['passcode'])): json_output("failure", "Passcode was empty."); endif;
 
-// Then process it and eithe reject it or sign in the user, update the session database, and make cookies
+$name_array = explode(" ", $_POST['name']);
+foreach ($name_array as $key_temp => $name_temp):
+  if (	ctype_space($name_temp) || 
+				empty($name_temp) || 
+				(strlen($name_temp) < 2) || 
+				($name_temp == "and") ):
+		unset($name_array[$key_temp]);
+		continue; endif;
+	$name_array[$key_temp] = str_replace([".", ","], null, $name_temp);
+  endforeach;
+if (count($name_array) < 3): json_output("failure", "Name too short."); endif;
+if (count($name_array) > 3): json_output("failure", "Name too long."); endif;
 
-// And give the appropriate response, or redirect
+
+// Identify the name...
+
+
+
+// Check passcode against name...
+
+
+
+// If signin failed, then just output a failure...
+
+
+// If signin passed, then update the session database and make cookies and give a positive response and redirect...
+
+
 
 ?>
