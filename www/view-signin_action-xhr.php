3c5c4a325f78daa28471e053f53347335e9bc36e
diis-me <41995924+diis-me@users.noreply.github.com>
2018-12-05 01:54:51 +0300

    Update view-signin_action-xhr.php

--- www/view-signin_action-xhr.php
+++ www/view-signin_action-xhr.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Origin: https://diis.online");
 header("AMP-Access-Control-Allow-Source-Origin: https://diis.online");
 
-$_POST['name'] = "riotous sinking chamber";
+$_POST['name'] = "riotus sinking chaber";
 $_POST['passcode'] = "105123";
 
 // Check signin name
