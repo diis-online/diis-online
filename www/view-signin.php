bb7a93391436abfd720478247cbe61275a2d7af4
diis-me <41995924+diis-me@users.noreply.github.com>
2018-12-06 00:30:08 +0300

    Update view-signin.php

--- www/view-signin.php
+++ www/view-signin.php
@@ -13,16 +13,13 @@ echo '<amp-state id="input_name"><script type="application/json">{"input_name_va
 echo "<span class='signin-window-helper'>Enter your name.</span>";
 echo "<input id='signin-window-name-input' type='text' name='name' value='' [value]=\"input_name.input_name_value\" required>";
 
-// Later on, allow the form to receive a recommended spelling
-// And display a 'Did you mean...' button which updates the name input when you press it
-
 echo "<span class='signin-window-helper'>Enter your passcode.</span>";
 echo "<input id='signin-window-passcode-input' type='password' name='passcode' required>";
 
 echo "<br><span id='signin-window-signin-button' role='button' tabindex='0' on='tap:signin-window-form.submit'>". $translatable_elements['sign-in'][$language_request] .".</span>";
 
 echo "<div submit-success>aaaaa<span class='signin-window-submit-success'>bbbb<template type='amp-mustache'>ccc". $translatable_elements['success'][$language_request] ." {{{message}}}</template></span></div>";
-echo "<div submitting><span class='signin-window-submitting'>". $translatable_elements['sending-to-server'][$language_request] ."</span></div>";
 echo "<div submit-error><template type='amp-mustache'>{{{message}}}</template></div>";
+echo "<div submitting><span class='signin-window-submitting'>". $translatable_elements['sending-to-server'][$language_request] ."</span></div>";
 
 echo "</form>"; ?>
