a424bcd6f2a89ea900d4b2b2a5e4730554c83891
diis-me <41995924+diis-me@users.noreply.github.com>
2018-12-06 17:13:38 +0300

    Update view-register_action-usernames.php

--- www/view-register_action-usernames.php
+++ www/view-register_action-usernames.php
@@ -20,14 +20,25 @@ if (in_array($language_request, ["ar"])):
 	endif;
 
 // Get a list of all username options currently in the database...
-$username_options_array = [];
+$username_options_array = $words_array = [];
 $database_query = "SELECT * FROM username_options";
 $result = pg_query($database_connection, $database_query);
 while ($row = pg_fetch_assoc($result)):
 	if (empty($row[$language_request])): continue; endif;
 	if (empty($username_options_array[$row['part']])): $username_options_array[$row['part']] = []; endif;
-	$username_options_array[$row['part']][$row[$language_request]] = $row['option_id'];
+	$username_options_array[$row['part']][$row['option_id']] = $row[$language_request];
 	endwhile;
+
+// This is to find matches
+// If we have 'big red rock' it will catch 'big red rock' or 'red big rock'
+$array_temp = [
+	"(name_one=$1 OR name_one=$2)",
+	"(name_two=$1 OR name_two=$2)",
+	"(name_three=$3)",
+	];
+$statement_temp = "SELECT * FROM users WHERE ".implode(" AND ", $array_temp)";
+$result_temp = pg_prepare($database_connection, "check_users_statement", $statement_temp);
+if (database_result($result_temp) !== "success"): json_output("failure", "Database #176."); endif;
 
 $json_result = [ "items" => [] ];
 
@@ -40,44 +51,45 @@ while ($count_temp < 30):
 
 	$combined_temp = $adjective_quality_temp = $adjective_wildcard_temp = $noun_temp = null;
 
-	// Has to be a quality...
-	$cycle_temp = 0;
-	while (empty($adjective_quality_temp)):
-		$cycle_temp++;
-		$rand_temp = array_rand($username_options_array['adjective quality']);
-		$adjective_quality_temp = $username_options_array['adjective quality'][$rand_temp];
-		if (in_array($rand_temp, $used_array) && ($cycle_temp < 30)): $adjective_quality_temp = null;
-		else: $used_array[] = $combined_temp[] = $rand_temp; endif;
-		endwhile;
+	// One adjective has to be a quality...
+	$adjective_quality_temp = array_rand($username_options_array['adjective quality']);
 
-	// Can be either a quality or a color...
-	$cycle_temp = 0;						
-	while (empty($adjective_wildcard_temp)):
-		$cycle_temp++;
-		$options_temp = ["adjective quality", "adjective color"];
-		$option_temp = $options_temp[array_rand($options_temp)];
-		$rand_temp = array_rand($username_options_array[$option_temp]);
-		$adjective_wildcard_temp = $username_options_array[$option_temp][$rand_temp];
-		if (in_array($rand_temp, $used_array) && ($cycle_temp < 30)): $adjective_wildcard_temp = null;
-		else: $used_array[] = $combined_temp[] = $rand_temp; endif;
-		endwhile;
+	// Another wildcard adjective can be either a quality or a color...
+	$options_temp = ["adjective quality", "adjective color"];
+	$option_temp = $options_temp[array_rand($options_temp)];
+	$adjective_wildcard_temp = array_rand($username_options_array[$option_temp]);
 
-	// Has to be a noun...
-	$cycle_temp = 0;						
-	while (empty($noun_temp)):
-		$cycle_temp++;
-		$rand_temp = array_rand($username_options_array['noun']);
-		$noun_temp = $username_options_array['noun'][$rand_temp];
-		if (in_array($rand_temp, $used_array) && ($cycle_temp < 30)): $noun_temp = null;
-		else: $used_array[] = $combined_temp[] = $rand_temp; endif;
-		endwhile;
+	// And there must be a noun
+	$noun_temp = array_rand($username_options_array['noun']);
 
 	// Must be three words...
 	if (empty($adjective_quality_temp) || empty($adjective_wildcard_temp) || empty($noun_temp)): continue; endif;
 
-	// Names may not contain two nor three of the same words as any other name.		   
+	// Get the words themselves, not just IDs...
+	$adjective_quality_word_temp = $username_options_array['adjective quality'][$adjective_quality_temp] ?? null;
+	$adjective_wildcard_word_temp = $username_options_array['adjective quality'][$adjective_wildcard_temp] ?? $username_options_array['adjective color'][$adjective_wildcard_temp] ?? null;
+	$noun_word_temp = $username_options_array['noun'][$noun_temp] ?? null;
+
+	// Double check the words exist...
+	if (empty($adjective_quality_word_temp) || empty($adjective_wildcard_word_temp) || empty($noun_word_temp)): continue; endif;
+
+	// And the two adjectives cannot be the same...
+	if ($adjective_quality_temp == $adjective_wildcard_temp): continue; endif;
+
+	// And cannot exist already for another user...
+	$values_temp = [$adjective_quality_temp, $adjective_wildcard_temp, $noun_temp];
+	$result_temp = pg_execute($database_connection, "check_share_id_statement", $values_temp);
+	if (database_result($result_temp) !== "success"): json_output("failure", "Database #177."); endif;
+	while ($row_temp = pg_fetch_assoc($result_temp)):
+		continue 2;
+		endwhile;
+
+	// Alternatively, we can SELECT all users and do array_intersect to see if
+	// they have three (or even just two) words in common
+
+	// But if it passes all this then it can be used...
 	$json_result['items'][] = [
-		"combined" => username_combine($combined_temp[0], $combined_temp[1], $combined_temp[2], $language_request),
+		"combined" => username_combine($adjective_quality_word_temp, $adjective_wildcard_word_temp, $noun_word_temp, $language_request),
 		"name-one" => $adjective_quality_temp, 
 		"name-two" => $adjective_wildcard_temp, 
 		"name-three" => $noun_temp,
