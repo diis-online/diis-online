267fd0f2cd4326af4e20665deb21b5b22523e07a
diis-me <41995924+diis-me@users.noreply.github.com>
2018-12-13 01:28:48 +0300

    Update index.php

--- www/index.php
+++ www/index.php
@@ -120,6 +120,8 @@ function body($title="Diis", $include=null) {
 
 	global $database_connection;
 	
+	global $page_id;
+	
 	global $languages;
 	global $translatable_elements;
 	
@@ -178,6 +180,9 @@ function body($title="Diis", $include=null) {
 	echo "<link rel='shortcut icon' type='image/png' href='https://diis.online/browser-icon.png'>";
 	echo "<link rel='apple-touch-icon' type='image/png' href='https://diis.online/browser-icon.png'>";
 
+	// Instant articles
+	if (!(empty($page_id))): echo "<meta property='fb:pages' content='" . $page_id ."'>"; endif;
+	
 	// Fonts and CSS style
 	echo '<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">';
 	echo '<link href="https://fonts.googleapis.com/css?family=Libre+Baskerville" rel="stylesheet">';
